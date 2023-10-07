c722bb5f2cb40d318a23d0f44f22693f7df475a6
WebsiteAB <74362768+AdrianBaranWeb@users.noreply.github.com>
2023-10-07 18:56:12 +0200

    Add basic structure for schedules

---
+++ src/pages/app/SchedulesView/ScheduleForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ScheduleForm = () => {
+  return (
+    <section>
+      <h2 className="app__title">Dodaj nowy harmonogram</h2>
+      
+    </section>
+  )
+}
+
+export default ScheduleForm
---
+++ src/pages/app/SchedulesView/index.jsx
@@ -0,0 +1,51 @@
+// Librares
+import React, {useState} from 'react';
+// Components
+import AppLayout from '../../../component/AppLayout';
+import HeroHeader from '../../../component/HeroHeader';
+import Table from '../../../component/Table/Table';
+import ScheduleForm from './ScheduleForm';
+// Images
+import homeBg from '../../../assets/img/landscape.jpg';
+
+const PLACEHOLDER_TABLE = {
+	headers: [
+		{id: 'h01', value: 'Nazwa harmonogramu'},
+		{id: 'h02', value: 'Edytuj'},
+		{id: 'h03', value: 'Usuń'},
+	],
+	data: [
+		{
+			id: 'd01',
+			dataRow: [
+				{id: 'd01t01', value: 'Testowy temat'},
+				{id: 'd01t02', value: new Date().toLocaleString()},
+				{id: 'd01t06', value: '+48123123123'},
+			],
+		},
+	],
+};
+
+const SchedulesView = () => {
+	const [schedules, setSchedules] = useState([]);
+	// TODO: Add downloading schedules from API
+
+	return (
+		<AppLayout>
+			<HeroHeader
+				title={`Hrmonogramy`}
+				image={<img src={homeBg} alt='harmonogramy' />}
+			/>
+			<main className='schedule-view'>
+				<section>
+					<h2 className='app__title'>Lista harmonogramów</h2>
+					<Table {...PLACEHOLDER_TABLE} />
+				</section>
+
+				<ScheduleForm />
+			</main>
+		</AppLayout>
+	);
+};
+
+export default SchedulesView;

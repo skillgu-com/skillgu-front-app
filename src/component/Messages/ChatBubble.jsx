e5a5993f4a87f80d2a684981c8d01639f8a2c02e
WebsiteAB <74362768+AdrianBaranWeb@users.noreply.github.com>
2023-10-07 13:33:02 +0200

    Style conversation

--- src/component/Messages/ChatBubble.jsx
+++ src/component/Messages/ChatBubble.jsx
@@ -1,12 +1,19 @@
 import React, {useMemo} from 'react';
 
 const Message = (props) => {
-	const {message, userId} = props;
+	const {message, userId, date} = props;
 
-  // TODO: Add logic to find out is this user or addressee
+	// TODO: Add logic to find out is this user or addressee
 	const isUserMessage = useMemo(() => userId, [userId]);
 
-	return <p className='chat-bubble' data-is-user={isUserMessage}>{message}</p>;
+	return (
+		<div className='chat-bubble' data-is-user={isUserMessage}>
+			<p className='chat-bubble__date'>{date}</p>
+			<p className='chat-bubble__message' >
+				{message}
+			</p>
+		</div>
+	);
 };
 
 export default Message;
--- src/component/Messages/Conversation.jsx
+++ src/component/Messages/Conversation.jsx
@@ -15,7 +15,38 @@ const PLACEHOLDER_CONVERSATION = {
 			'https://cdn.pixabay.com/photo/2016/11/21/12/42/beard-1845166_1280.jpg',
 		status: 'online',
 	},
-	content: [{id: 'm01', userId: true, message: 'Witam!'},{id: 'm02', userId: false, message: 'Zacznijmy naukę!'}],
+	content: [
+		{id: 'm01', userId: true, message: 'Witam!', date: new Date().toLocaleDateString()},
+		{id: 'm02', userId: false, message: 'Zacznijmy naukę!', date: new Date().toLocaleDateString()},
+		{
+			id: 'm03',
+			userId: false,
+			message:
+				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem nemo nulla eos laudantium et consectetur earum maxime magnam rerum. Minima neque ab fugiat reiciendis magnam architecto alias eos officiis facilis?',
+			date: new Date().toLocaleDateString(),
+		},
+		{
+			id: 'm06',
+			userId: false,
+			message:
+				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem nemo nulla eos laudantium et consectetur earum maxime magnam rerum. Minima neque ab fugiat reiciendis magnam architecto alias eos officiis facilis?',
+			date: new Date().toLocaleDateString(),
+		},
+		{
+			id: 'm04',
+			userId: true,
+			message:
+				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem nemo nulla eos laudantium et consectetur earum maxime magnam rerum.',
+			date: new Date().toLocaleDateString(),
+		},
+		{
+			id: 'm05',
+			userId: true,
+			message:
+				'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem nemo nulla eos laudantium et consectetur earum maxime magnam rerum.',
+			date: new Date().toLocaleDateString(),
+		},
+	],
 };
 
 const Conversation = (props) => {
@@ -35,22 +66,26 @@ const Conversation = (props) => {
 				name={conversation.user.username}
 				status={conversation.user.status}
 			/>
-			<div className='conversation__content'>
-				{conversation.content.map((message) => (
-					<ChatBubble key={message.id} {...message} />
-				))}
+			<div className='conversation__wrapper'>
+				<div className='conversation__content'>
+					{conversation.content.map((message) => (
+						<ChatBubble key={message.id} {...message} />
+					))}
+				</div>
 			</div>
 			<form className='conversation__form'>
-				<div className='book-form__group'>
-					<label className='book-form__text' htmlFor='message'>
-						<b>Wiadomość do</b>
-					</label>
-					<textarea name='message' id='message' cols='30' rows='10'></textarea>
+				<textarea
+					className='conversation__form-input'
+					name='message'
+					id='message'
+					cols='30'
+					rows='10'></textarea>
+				<div className='conversation__form-buttons'>
+					<CustomButton as={buttonTypes.button} color={buttonColors.secondary}>
+						Dodaj plik
+					</CustomButton>
+					<CustomButton as={buttonTypes.submit}>Wyślij</CustomButton>
 				</div>
-				<CustomButton as={buttonTypes.button} color={buttonColors.secondary}>
-					Dodaj plik
-				</CustomButton>
-				<CustomButton as={buttonTypes.submit}>Wyślij</CustomButton>
 			</form>
 		</section>
 	);

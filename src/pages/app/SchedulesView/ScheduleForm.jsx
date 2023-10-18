0616cb60b5ed02027c4449694b90d9ad0d22d31d
WebsiteAB <74362768+AdrianBaranWeb@users.noreply.github.com>
2023-10-18 22:54:52 +0200

    Optimize JSX

--- src/pages/app/SchedulesView/ScheduleForm.jsx
+++ src/pages/app/SchedulesView/ScheduleForm.jsx
@@ -1,6 +1,6 @@
 // Libraries
 import React, {useState} from 'react';
-import {FormLabel, TextField, Grid, Box, Tabs, Tab} from '@mui/material';
+import {Box, Tabs, Tab} from '@mui/material';
 // Components
 import CustomButton, {buttonTypes} from '../../../component/CustomButton';
 import Input from '../../../component/Input';
@@ -12,32 +12,142 @@ const INITIAL_STATE = {
 		errorMessage: '',
 		isValid: undefined,
 	},
-	email: {
+	meetTime: {
+		value: 45,
+		errorMessage: '',
+		isValid: undefined,
+	},
+	break: {
+		value: 0,
+		errorMessage: '',
+		isValid: undefined,
+	},
+	planned: {
 		value: '',
 		errorMessage: '',
 		isValid: undefined,
 	},
-	phone: {
+	limit: {
 		value: '',
 		errorMessage: '',
 		isValid: undefined,
 	},
-	company: {
+	start: {
 		value: '',
 		errorMessage: '',
 		isValid: undefined,
 	},
-	message: {
+	end: {
 		value: '',
 		errorMessage: '',
 		isValid: undefined,
 	},
-	policy: {
-		value: false,
+	timeMonday: {
+		value: '',
+		errorMessage: '',
+		isValid: undefined,
+	},
+	timeTuesday: {
+		value: '',
+		errorMessage: '',
+		isValid: undefined,
+	},
+	timeWednesday: {
+		value: '',
+		errorMessage: '',
+		isValid: undefined,
+	},
+	timeThursday: {
+		value: '',
+		errorMessage: '',
+		isValid: undefined,
+	},
+	timeFriday: {
+		value: '',
+		errorMessage: '',
+		isValid: undefined,
+	},
+	timeSaturday: {
+		value: '',
+		errorMessage: '',
+		isValid: undefined,
+	},
+	timeSunday: {
+		value: '',
 		errorMessage: '',
 		isValid: undefined,
 	},
 };
+
+const inputs = [
+	{
+		id: 'name',
+		name: 'name',
+		type: 'text',
+		placeholder: 'Mój harmonogram',
+		label: 'Nazwa harmonogramu',
+		required: true,
+	},
+	{
+		id: 'meetTime',
+		name: 'meetTime',
+		type: 'number',
+		label: 'Długość spotkania',
+		required: true,
+		step: '5',
+		min: 15,
+	},
+	{
+		id: 'break',
+		name: 'break',
+		type: 'number',
+		label: 'Długość przerwy',
+		placeholder: '15',
+		required: true,
+		step: '15',
+		min: 0,
+	},
+	{
+		id: 'planned',
+		name: 'planned',
+		type: 'number',
+		label: 'Spotaknie może być zaplanowane nie wcześniej niż',
+		placeholder: 'Pozostaw puste jeżeli chcesz zdjąć ograniczenia',
+		required: false,
+	},
+	{
+		id: 'limit',
+		name: 'limit',
+		type: 'number',
+		label: 'Limit dziennych spotkań',
+		placeholder: 'Pozostaw puste jeżeli chcesz zdjąć ograniczenia',
+		required: false,
+	},
+	{
+		id: 'start',
+		name: 'start',
+		type: 'date',
+		label: 'Rozpoczęcie harmonogramu',
+		required: true,
+	},
+	{
+		id: 'end',
+		name: 'end',
+		type: 'date',
+		label: 'Zakończenie harmonogramu',
+		required: true,
+	},
+];
+
+const inputsHours = [
+	{id: 'timeMonday', name: 'timeMonday', label: 'Poniedziałek'},
+	{id: 'timeTuesday', name: 'timeTuesday', label: 'Wtorek'},
+	{id: 'timeWednesday', name: 'timeWednesday', label: 'Środa'},
+	{id: 'timeThursday', name: 'timeThursday', label: 'Czwartek'},
+	{id: 'timeFriday', name: 'timeFriday', label: 'Piątek'},
+	{id: 'timeSaturday', name: 'timeSaturday', label: 'Sobota'},
+	{id: 'timeSunday', name: 'timeSunday', label: 'Niedziela'},
+];
 
 function TabPanel(props) {
 	const {children, value, index, ...other} = props;
@@ -82,7 +192,7 @@ const ScheduleForm = () => {
 			<h2 className='app__title'>Dodaj nowy harmonogram</h2>
 
 			<form className='schedule-form' onSubmit={submitHandler}>
-				<Box sx={{width: '100%'}} marginTop={4}>
+				<Box sx={{width: '100%'}}>
 					<Box sx={{borderBottom: 1, borderColor: 'divider'}}>
 						<Tabs
 							value={value}
@@ -93,98 +203,24 @@ const ScheduleForm = () => {
 						</Tabs>
 					</Box>
 					<TabPanel value={value} index={0}>
-						<Input
-							id='name'
-							name='name'
-							type='text'
-							placeholder='Mój harmonogram'
-							label='Nazwa harmonogramu'
-							required={true}
-							value={currentState.name.value}
-							errorMessage={currentState.name.errorMessage}
-							valueChangeHandler={upadateStateHandler}
-						/>
-						<Input
-							id='break'
-							name='break'
-							type='number'
-							placeholder='15'
-							label='Długość przerwy'
-							// required={true}
-							value={currentState.name.value}
-							errorMessage={currentState.name.errorMessage}
-							valueChangeHandler={upadateStateHandler}
-						/>
-						<Input
-							id='break'
-							name='break'
-							type='number'
-							placeholder='15'
-							label='Spotaknie może być zaplanowane nie wcześniej niż'
-							// required={true}
-							value={currentState.name.value}
-							errorMessage={currentState.name.errorMessage}
-							valueChangeHandler={upadateStateHandler}
-						/>
-						<Input
-							id='limit'
-							name='limit'
-							type='number'
-							placeholder='Pozostaw puste jeżeli chcesz zdjąć ograniczenia'
-							label='Limit dziennych spotkań'
-							// required={true}
-							value={currentState.name.value}
-							errorMessage={currentState.name.errorMessage}
-							valueChangeHandler={upadateStateHandler}
-						/>
-						<h3 className='app__title'>Harmonogram obowiązuje</h3>
-						<div className='schedule-form__inline'>
-							<Grid item xs={12} md={6}>
-								<FormLabel id='start-data' className='field__label'>
-									Od
-								</FormLabel>
-								<TextField
-									autoComplete='given-name'
-									name='start-data'
-									required
-									fullWidth
-									id='start-data'
-									placeholder='12.02.2022'
-									type={'date'}
-									autoFocus
-									// value={}
-									// onChange={props.handleStartDate}
-								/>
-							</Grid>
-							<Grid item xs={12} md={6}>
-								<FormLabel id='start-data' className='field__label'>
-									Do
-								</FormLabel>
-								<TextField
-									autoComplete='given-name'
-									name='start-data'
-									required
-									fullWidth
-									id='start-data'
-									placeholder='12.02.2022'
-									type={'date'}
-									autoFocus
-									// value={}
-									// onChange={props.handleStartDate}
-								/>
-							</Grid>
-						</div>
+						{inputs.map((input) => (
+							<Input
+								{...input}
+								value={currentState[input.name].value}
+								errorMessage={currentState[input.name].errorMessage}
+								valueChangeHandler={upadateStateHandler}
+							/>
+						))}
 					</TabPanel>
 					<TabPanel value={value} index={1}>
-						<Hours
-							id='limit'
-							name='limit'
-							placeholder='Pozostaw puste jeżeli chcesz zdjąć ograniczenia'
-							label='Poniedziałek'
-							value={currentState.name.value}
-							errorMessage={currentState.name.errorMessage}
-							valueChangeHandler={upadateStateHandler}
-						/>
+						{inputsHours.map((input) => (
+							<Hours
+								{...input}
+								value={currentState[input.name].value}
+								errorMessage={currentState[input.name].errorMessage}
+								valueChangeHandler={upadateStateHandler}
+							/>
+						))}
 					</TabPanel>
 				</Box>
 

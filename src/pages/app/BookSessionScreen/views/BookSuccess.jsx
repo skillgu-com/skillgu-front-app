6eb2a9c06d596beb32ed3ce060932e3f2f5f24c9
WebsiteAB <74362768+AdrianBaranWeb@users.noreply.github.com>
2023-11-20 23:28:46 +0100

    Add calendar button

--- src/pages/app/BookSessionScreen/views/BookSuccess.jsx
+++ src/pages/app/BookSessionScreen/views/BookSuccess.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import Confetti from 'react-confetti';
 import CustomButton, {buttonTypes} from '../../../../component/CustomButton';
+import {AddToCalendarButton} from 'add-to-calendar-button-react';
+import {useSelector} from 'react-redux';
 
 const BookSuccess = () => {
-  console.log(buttonTypes.internalLink);
+	const userFromRedux = useSelector(
+		(state) => state.connectionProcess.sessionStep
+	);
+
 	return (
 		<div className='book-success'>
 			<Confetti
@@ -14,9 +19,18 @@ const BookSuccess = () => {
 			<h2 className='book-success__title book-payment__submit-session-price'>
 				Twoje spotkanie zostało zarezerwowane!
 			</h2>
-			<CustomButton as={buttonTypes.internalLink} link={'/home'}>
-				Powrót do strony głównej
-			</CustomButton>
+			<div className='book-success__button'>
+				<AddToCalendarButton
+					name='Title'
+					options={['Apple', 'Google']}
+					location='World Wide Web'
+					startDate='2023-11-23'
+					endDate='2023-11-23'
+					startTime='10:15'
+					endTime='23:30'
+					timeZone='America/Los_Angeles'
+				/>
+			</div>
 		</div>
 	);
 };

3a9b4cf1a6334683e5c9feff726d6120558f8da3
WebsiteAB <74362768+AdrianBaranWeb@users.noreply.github.com>
2023-12-20 00:30:20 +0100

    Add redirections

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
+// Libraries
+import {Navigate} from 'react-router-dom';
 // Pages
-import Landing from './pages/LandingPage';
 import RegisterPage from './pages/RegisterPage/RegisterPage';
 import LoginPage from './pages/LoginPage/LoginPage';
 import RemindPasswordPage from './pages/RemindPasswordPage/RemindPasswordPage';
@@ -24,174 +25,180 @@ import AccountView from './pages/app/AccountView/AccountView';
 import MentoringPlanCreatorView from './pages/app/CreateMeetingView/FormSteps/MentoringPlanCreatorView';
 import SessionPlanCreatorView from './pages/app/CreateMeetingView/FormSteps/SessionPlanCreatorViews';
 import AccountSettlement from './pages/app/AccountSettlement/AccountSettlement';
-import LoggedProfileScreen from "./pages/app/UserProfileScreen/LoggedProfileScreen";
-import MessagesView from "./pages/app/MessagesView/MessagesView";
-import SchedulesView from "./pages/app/SchedulesView";
+import LoggedProfileScreen from './pages/app/UserProfileScreen/LoggedProfileScreen';
+import MessagesView from './pages/app/MessagesView/MessagesView';
+import SchedulesView from './pages/app/SchedulesView';
 
 const routes = [
 	{
 		id: 'route01',
-    path: '/',
-    element: <Landing />,
+		path: '/',
+		element: <Navigate to='/home' />,
 	},
 	{
 		id: 'route02',
-    path: '/login',
-    element: <LoginPage />,
+		path: '/login',
+		element: <LoginPage />,
 	},
 	{
 		id: 'route029',
-    path: '/remind-password',
-    element: <RemindPasswordPage />,
+		path: '/remind-password',
+		element: <RemindPasswordPage />,
 	},
 	{
 		id: 'route03',
-    path: '/register',
-    element: <RegisterPage />,
-	},{
+		path: '/register',
+		element: <RegisterPage />,
+	},
+	{
 		id: 'route04',
-    path: '/home',
-    element: <HomeScreen />,
+		path: '/home',
+		element: <HomeScreen />,
 		isProtected: true,
-	},{
+	},
+	{
 		id: 'route05',
-    path: '/create-mentor',
-    element: <PitchDeckCreatorScreen />,
+		path: '/create-mentor',
+		element: <PitchDeckCreatorScreen />,
 		isProtected: true,
-	},{
+	},
+	{
 		id: 'route06',
-    path: '/search-invest-projects',
-    element: <ProjectsSearchScreen />,
+		path: '/search-invest-projects',
+		element: <ProjectsSearchScreen />,
 		isProtected: true,
-	},{
+	},
+	{
 		id: 'route07',
-    path: '/search-business-partner',
-    element: <SearchBusinessPartner />,
+		path: '/search-business-partner',
+		element: <SearchBusinessPartner />,
 		isProtected: true,
-	},{
+	},
+	{
 		id: 'route08',
-    path: '/invest-maps',
-    element: <ProjectsMapScreen />,
+		path: '/invest-maps',
+		element: <ProjectsMapScreen />,
 		isProtected: true,
-	},{
+	},
+	{
 		id: 'route09',
-    path: '/mentors',
-    element: <MentorScreen />,
+		path: '/mentors',
+		element: <MentorScreen />,
 		isProtected: true,
 	},
-  {
+	{
 		id: 'route10',
-    path: '/user-profile',
-    element: <LoggedProfileScreen />,
+		path: '/user-profile',
+		element: <LoggedProfileScreen />,
 		isProtected: true,
 	},
-  {
+	{
 		id: 'route11',
-    path: '/user-profile/:userID',
-    element: <UserProfileScreen />,
+		path: '/user-profile/:userID',
+		element: <UserProfileScreen />,
 		isProtected: true,
 	},
-  {
+	{
 		id: 'route12',
-    path: '/mentor-profile/:userID',
-    element: <UserProfileScreen />,
+		path: '/mentor-profile/:userID',
+		element: <UserProfileScreen />,
 		isProtected: true,
 	},
-  {
+	{
 		id: 'route13',
-    path: '/session-details/:id/book',
-    element: <BookSessionScreen />,
+		path: '/session-details/:id/book',
+		element: <BookSessionScreen />,
 		isProtected: true,
 	},
-  {
+	{
 		id: 'route14',
-    path: '/session-details/:mentorID',
-    element: <SessionDetailsScreen />,
+		path: '/session-details/:mentorID',
+		element: <SessionDetailsScreen />,
 		isProtected: true,
 	},
-  {
+	{
 		id: 'route15',
-    path: '/business-partner',
-    element: <Underconstruction />,
+		path: '/business-partner',
+		element: <Underconstruction />,
 		isProtected: true,
 	},
-  {
+	{
 		id: 'route16',
-    path: '/help',
-    element: <HelpScreen />,
+		path: '/help',
+		element: <HelpScreen />,
 		isProtected: true,
 	},
-  {
-    id: 'route17',
-    path: '/underconstruction',
-    element: <Underconstruction />,
+	{
+		id: 'route17',
+		path: '/underconstruction',
+		element: <Underconstruction />,
 	},
-  {
-    id: 'route18',
-    path: '/user-setup',
-    element: <Settings />,
-    isProtected: true,
+	{
+		id: 'route18',
+		path: '/user-setup',
+		element: <Settings />,
+		isProtected: true,
 	},
-  {
-    id: 'route19',
-    path: '/calendar-view',
-    element: <CalendarView />,
-    isProtected: true,
+	{
+		id: 'route19',
+		path: '/calendar-view',
+		element: <CalendarView />,
+		isProtected: true,
 	},
-  {
-    id: 'route20',
-    path: '/sessions-mentorings',
-    element: <CreateNewMeeting />,
-    isProtected: true,
+	{
+		id: 'route20',
+		path: '/sessions-mentorings',
+		element: <CreateNewMeeting />,
+		isProtected: true,
 	},
-  {
-    id: 'route21',
-    path: '/schedule-meeting',
-    element: <MeetingSchedule />,
-    isProtected: true,
+	{
+		id: 'route21',
+		path: '/schedule-meeting',
+		element: <MeetingSchedule />,
+		isProtected: true,
 	},
-  {
-    id: 'route22',
-    path: '/account-view',
-    element: <AccountView />,
-    isProtected: true,
+	{
+		id: 'route22',
+		path: '/account-view',
+		element: <AccountView />,
+		isProtected: true,
 	},
-  {
-    id: 'route23',
-    path: '/create-mentoring',
-    element: <MentoringPlanCreatorView />,
-    isProtected: true,
+	{
+		id: 'route23',
+		path: '/create-mentoring',
+		element: <MentoringPlanCreatorView />,
+		isProtected: true,
 	},
-  {
-    id: 'route24',
-    path: '/create-session',
-    element: <SessionPlanCreatorView />,
-    isProtected: true,
+	{
+		id: 'route24',
+		path: '/create-session',
+		element: <SessionPlanCreatorView />,
+		isProtected: true,
 	},
-  {
-    id: 'route25',
-    path: '/raports',
-    element: <Raports />,
-    isProtected: true,
+	{
+		id: 'route25',
+		path: '/raports',
+		element: <Raports />,
+		isProtected: true,
 	},
-  {
-    id: 'route26',
-    path: '/account-settlements',
-    element: <AccountSettlement />,
-    isProtected: true,
+	{
+		id: 'route26',
+		path: '/account-settlements',
+		element: <AccountSettlement />,
+		isProtected: true,
 	},
-  {
-    id: 'route27',
-    path: '/messages',
-    element: <MessagesView />,
-    isProtected: true,
+	{
+		id: 'route27',
+		path: '/messages',
+		element: <MessagesView />,
+		isProtected: true,
 	},
-  {
-    id: 'route28',
-    path: '/schedules',
-    element: <SchedulesView />,
-    isProtected: true,
+	{
+		id: 'route28',
+		path: '/schedules',
+		element: <SchedulesView />,
+		isProtected: true,
 	},
 ];
 
-export default routes
+export default routes;

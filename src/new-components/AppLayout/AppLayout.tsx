e5401e1623b4ae70a844b9a8e6df740b98a97130
Adrian Baran <74362768+AdrianBaranWeb@users.noreply.github.com>
2023-12-28 23:40:13 +0100

    Add basic notifications

--- src/new-components/AppLayout/AppLayout.tsx
+++ src/new-components/AppLayout/AppLayout.tsx
@@ -2,17 +2,21 @@
 import React from 'react';
 // Components
 import Navbar from './components/Navbar/Navbar';
+import Notifications from './components/Notifications/Notifications';
 // Types
 import {Common} from '../../types/main';
 // Styles
-import styles from './AppLayout.module.scss'
+import styles from './AppLayout.module.scss';
 
 const AppLayout = (props: Common) => {
 	const {children} = props;
 	return (
 		<>
 			<Navbar />
-			<div className={styles.content}>{children}</div>
+			<div className={styles.content}>
+				<Notifications />
+				{children}
+			</div>
 		</>
 	);
 };

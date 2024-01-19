911533f6ca7d1ac19eae8d4f39726da856d281f6
Adrian Baran <74362768+AdrianBaranWeb@users.noreply.github.com>
2024-01-19 18:54:32 +0100

    Add accountType hook

--- src/pages/app/Settings/Settings.tsx
+++ src/pages/app/Settings/Settings.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 // Components
 import AppHeader from 'src/new-components/AppHeader/AppHeader';
 import Container from 'src/new-components/Container/Container';
+// Hooks
+import {useAccountType} from 'src/hooks/useAccountType';
 // Sections
 import DeleteAccount from './sections/DeleteAccount/DeleteAccount';
 import Specification from './sections/Specyfication/Specification';
@@ -14,12 +16,14 @@ import {Tag} from 'src/types/tags';
 import styles from './Settings.module.scss';
 
 const Settings = () => {
+	const {isMentor} = useAccountType();
+
 	return (
 		<>
 			<AppHeader title='Ustawienia użytownika' text='Zarządzaj swoimi danymi.' />
 			<Container as={Tag.Main} classes={styles.wrapper}>
 				<YourData />
-				<Specification />
+				{isMentor && <Specification />}
 				<ChangePassword />
 				<DeleteAccount />
 			</Container>

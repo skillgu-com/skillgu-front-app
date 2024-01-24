9efabad07007582d42850cf74c06620b77c26db0
maciejmaciek <hello@maciejglazer.com>
2024-01-24 17:13:11 +0100

    update

--- src/pages/app/SearchMentors/sections/Filters/Filters.tsx
+++ src/pages/app/SearchMentors/sections/Filters/Filters.tsx
@@ -13,166 +13,183 @@ import FilterSvg from 'src/assets/icons/FilterSvg';
 import SearchSvg from 'src/assets/icons/SearchSvg';
 // API
 import {getAllMentors} from 'src/services/UserProfileService';
-import {getAllFilteredMentors} from 'src/services/MentorViewService';
+import {getAllFilteredMentors, getAllMentorCategories} from 'src/services/MentorViewService';
 // Styles
 import styles from './Filters.module.scss';
 // Types
 import {Tag} from 'src/types/tags';
 import {
-	TitleTag,
-	TitleVariant,
+    TitleTag,
+    TitleVariant,
 } from 'src/new-components/typography/Title/Title';
 import Select from 'src/new-components/Select/Select';
 import {defaultInput} from 'src/new-components/Input/Input';
 
+interface BackendItem {
+    id: number;
+    name: string;
+}
+
+interface TransformedItem {
+    id: string;
+    name: string;
+    label: string;
+    // Dodaj tutaj inne pola, które masz w defaultInput
+}
+
+type TransformedData = {
+    [key: string]: TransformedItem;
+};
 const Filters = () => {
-	const [filtersModal, setFiltersModal] = useState(false);
+    const [filtersModal, setFiltersModal] = useState(false);
 
-	const [form, setForm] = useState({
-		mentorGroup: {
-			...defaultInput,
-			value: {
-				test: {id: 'test', name: 'test', label: 'IT', ...defaultInput},
-				test1: {id: 'test1', name: 'test1', label: 'IT', ...defaultInput},
-				test2: {id: 'test2', name: 'test2', label: 'IT', ...defaultInput},
-				test3: {id: 'test3', name: 'test3', label: 'IT', ...defaultInput},
-				test4: {id: 'test3', name: 'test3', label: 'IT', ...defaultInput},
-			},
-		},
-		languages: {
-			...defaultInput,
-			value: {
-				test: {id: 'test', name: 'test', label: 'IT', ...defaultInput},
-				test1: {id: 'test1', name: 'test1', label: 'IT', ...defaultInput},
-				test2: {id: 'test2', name: 'test2', label: 'IT', ...defaultInput},
-				test3: {id: 'test3', name: 'test3', label: 'IT', ...defaultInput},
-				test4: {id: 'test3', name: 'test3', label: 'IT', ...defaultInput},
-			},
-		},
-		timeZone: {
-			...defaultInput,
-			value: {
-				test: {id: 'test', name: 'test', label: 'IT', ...defaultInput},
-				test1: {id: 'test1', name: 'test1', label: 'IT', ...defaultInput},
-				test2: {id: 'test2', name: 'test2', label: 'IT', ...defaultInput},
-				test3: {id: 'test3', name: 'test3', label: 'IT', ...defaultInput},
-				test4: {id: 'test3', name: 'test3', label: 'IT', ...defaultInput},
-			},
-		},
-		price: {...defaultInput, value: [0, 100]},
-		password: defaultInput,
-	});
+    const transformBackendData = (backendData: BackendItem[]): TransformedData => {
+        const transformedData: TransformedData = {};
 
-	const updateFormHandler = (name: string, value: any) => {
-		console.log('jestem tutaj:!')
-		setForm({...form, [name]: value});
-	};
+        backendData.forEach(item => {
+            transformedData[item.id.toString()] = {
+                id: item.id.toString(),
+                name: item.name,
+                label: item.name,
+                ...defaultInput
+            };
+        });
 
-	const submitHandler = (e: FormEvent) => {
-		e.preventDefault();
-	};
+        return transformedData;
+    };
 
-	return (
-		<Container as={Tag.Section} classes={styles.wrapper}>
-			<Title tag={TitleTag.h2} variant={TitleVariant.standard}>
-				Filtr wyszykiwań
-			</Title>
-			<div className={styles.filtersGroup}>
-				<form onSubmit={submitHandler} className={styles.search}>
-					<input type='text' placeholder='Szukaj po umiejetnosci lub tytule pracy' />
-					<button type='submit'>
-						<SearchSvg />
-					</button>
-				</form>
-				<Select
-					options={[
-						{value: 'alphabet', label: 'Alfabetycznie'},
-						{value: 'increase', label: 'Rosnąco'},
-						{value: 'next', label: 'Kolejna'},
-					]}
-					value={'alphabet'}
-					valueChangeHandler={updateFormHandler}
-					name='sort'
-					id='sort'
-					label='Sortowanie'
-				/>
-				<Select
-					options={[
-						{value: 'alphabet', label: 'Alfabetycznie'},
-						{value: 'increase', label: 'Rosnąco'},
-						{value: 'next', label: 'Kolejna'},
-					]}
-					value={'alphabet'}
-					valueChangeHandler={updateFormHandler}
-					name='categories'
-					id='categories'
-					label='Kategorie'
-				/>
-				<Select
-					options={[
-						{value: 'alphabet', label: 'Alfabetycznie'},
-						{value: 'increase', label: 'Rosnąco'},
-						{value: 'next', label: 'Kolejna'},
-					]}
-					value={'alphabet'}
-					valueChangeHandler={updateFormHandler}
-					name='skills'
-					id='skills'
-					label='Umiejętności'
-				/>
-				<button
-					className={styles.more}
-					onClick={() => {
-						setFiltersModal(true);
-					}}>
-					Więcej filtrów <FilterSvg />
-				</button>
-				<button className={styles.clear}>Wyczyść filtry</button>
-			</div>
-			{filtersModal && (
-				<Modal
-					closeHandler={() => {
-						setFiltersModal(false);
-					}}
-					title='Więcej filtrów wyszukiwania'>
-					<MulitSelect
-						name='mentorGroup'
-						limit={4}
-						classes={styles.select}
-						value={form.mentorGroup.value}
-						label='Grupa mentorów'
-						onValueChange={updateFormHandler}
-					/>
-					<MulitSelect
-						name='timeZone'
-						limit={4}
-						classes={styles.select}
-						value={form.timeZone.value}
-						label='Strefa czasowa'
-						onValueChange={updateFormHandler}
-					/>
-					<MulitSelect
-						name='languages'
-						limit={4}
-						classes={styles.select}
-						value={form.languages.value}
-						label='Język'
-						onValueChange={updateFormHandler}
-					/>
-					<RangeInput
-						id='price'
-						label='Stawka'
-						minValue={0}
-						maxValue={100}
-						currentMinValue={form.price.value[0]}
-						currentMaxValue={form.price.value[1]}
-						valueChangeHandler={updateFormHandler}
-					/>
-					<Button classes={styles.button}>Wyszukaj</Button>
-				</Modal>
-			)}
-		</Container>
-	);
+    useEffect(() => {
+        getAllMentorCategories().then(res => {
+            const transformedData = transformBackendData(res.data);
+            setForm(prevForm => ({
+                ...prevForm,
+                mentorGroup: {
+                    ...prevForm.mentorGroup,
+                    value: transformedData
+                }
+            }));
+        }).catch(error => {
+            console.error("Błąd podczas ładowania kategorii mentorów:", error);
+        });
+    }, []);
+
+    const [form, setForm] = useState({
+        mentorGroup: {
+            ...defaultInput,
+            value: {}
+        },
+        // inne pola...
+    });
+
+    const updateFormHandler = (name: string, value: any) => {
+        setForm({...form, [name]: value});
+    };
+
+    const submitHandler = (e: FormEvent) => {
+        e.preventDefault();
+    };
+
+    return (
+        <Container as={Tag.Section} classes={styles.wrapper}>
+            <Title tag={TitleTag.h2} variant={TitleVariant.standard}>
+                Filtr wyszykiwań
+            </Title>
+            <div className={styles.filtersGroup}>
+                <form onSubmit={submitHandler} className={styles.search}>
+                    <input type='text' placeholder='Szukaj po umiejetnosci lub tytule pracy'/>
+                    <button type='submit'>
+                        <SearchSvg/>
+                    </button>
+                </form>
+                <Select
+                    options={[
+                        {value: 'alphabet', label: 'Alfabetycznie'},
+                        {value: 'increase', label: 'Rosnąco'},
+                        {value: 'next', label: 'Kolejna'},
+                    ]}
+                    value={'alphabet'}
+                    valueChangeHandler={updateFormHandler}
+                    name='sort'
+                    id='sort'
+                    label='Sortowanie'
+                />
+                <Select
+                    options={[
+                        {value: 'alphabet', label: 'Alfabetycznie'},
+                        {value: 'increase', label: 'Rosnąco'},
+                        {value: 'next', label: 'Kolejna'},
+                    ]}
+                    value={'alphabet'}
+                    valueChangeHandler={updateFormHandler}
+                    name='categories'
+                    id='categories'
+                    label='Kategorie'
+                />
+                <Select
+                    options={[
+                        {value: 'alphabet', label: 'Alfabetycznie'},
+                        {value: 'increase', label: 'Rosnąco'},
+                        {value: 'next', label: 'Kolejna'},
+                    ]}
+                    value={'alphabet'}
+                    valueChangeHandler={updateFormHandler}
+                    name='skills'
+                    id='skills'
+                    label='Umiejętności'
+                />
+                <button
+                    className={styles.more}
+                    onClick={() => {
+                        setFiltersModal(true);
+                    }}>
+                    Więcej filtrów <FilterSvg/>
+                </button>
+                <button className={styles.clear}>Wyczyść filtry</button>
+            </div>
+            {filtersModal && (
+                <Modal
+                    closeHandler={() => {
+                        setFiltersModal(false);
+                    }}
+                    title='Więcej filtrów wyszukiwania'>
+                    <MulitSelect
+                        name='mentorGroup'
+                        limit={4}
+                        classes={styles.select}
+                        value={form.mentorGroup.value}
+                        label='Grupa mentorów'
+                        onValueChange={updateFormHandler}
+                    />
+                    {/*<MulitSelect*/}
+                    {/*    name='timeZone'*/}
+                    {/*    limit={4}*/}
+                    {/*    classes={styles.select}*/}
+                    {/*    value={form.timeZone.value}*/}
+                    {/*    label='Strefa czasowa'*/}
+                    {/*    onValueChange={updateFormHandler}*/}
+                    {/*/>*/}
+                    {/*<MulitSelect*/}
+                    {/*    name='languages'*/}
+                    {/*    limit={4}*/}
+                    {/*    classes={styles.select}*/}
+                    {/*    value={form.languages.value}*/}
+                    {/*    label='Język'*/}
+                    {/*    onValueChange={updateFormHandler}*/}
+                    {/*/>*/}
+                    {/*<RangeInput*/}
+                    {/*    id='price'*/}
+                    {/*    label='Stawka'*/}
+                    {/*    minValue={0}*/}
+                    {/*    maxValue={100}*/}
+                    {/*    currentMinValue={form.price.value[0]}*/}
+                    {/*    currentMaxValue={form.price.value[1]}*/}
+                    {/*    valueChangeHandler={updateFormHandler}*/}
+                    {/*/>*/}
+                    <Button classes={styles.button}>Wyszukaj</Button>
+                </Modal>
+            )}
+        </Container>
+    );
 };
 
 export default Filters;

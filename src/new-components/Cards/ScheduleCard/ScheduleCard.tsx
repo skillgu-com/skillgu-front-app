ba56c19a86f715d19fea56a959846892e44662a1
Adrian Baran <74362768+AdrianBaranWeb@users.noreply.github.com>
2024-03-09 17:53:19 +0100

    Add styled Sessions and Schedules

---
+++ src/new-components/Cards/ScheduleCard/ScheduleCard.tsx
@@ -0,0 +1,86 @@
+import React from 'react';
+// Components
+import Options from './components/Options/Options';
+// Icons
+import Money from './components/icons/Money';
+import Calendar from './components/icons/Calendar';
+import Timer from './components/icons/Timer';
+import Person from './components/icons/Person';
+// Styles
+import styles from './ShceduleCard.module.scss';
+// Types
+import { Option } from './components/Options/Options';
+
+export interface ScheduleCardProps {
+	id: string;
+	dateStart: Date;
+	dateEnd: Date;
+	time: number;
+	title: string;
+  options: Option[]
+	schedule?: {
+		type: 'individual' | 'group';
+		created: Date;
+		assignedSessions: number;
+	};
+	session?: {
+		description: string;
+		price: number;
+	};
+}
+
+const ScheduleCard = (props: ScheduleCardProps) => {
+	if (!!!props.schedule && !!!props.session)
+		throw new Error('One of parameters schedule or session is required!');
+
+	const {id, dateStart, dateEnd, time, title, schedule, session, options} = props;
+
+	return (
+		<div className={styles.wrapper}>
+			<div className={styles.header}>
+				<h3 className={styles.title}>{title}</h3>
+        <Options options={options}/>
+			</div>
+			{!!schedule && (
+				<>
+					<p>Data stworzenia: {schedule.created.toLocaleDateString()}</p>
+				</>
+			)}
+			{!!schedule && (
+				<p data-is-assigned={schedule.assignedSessions !== 0}>
+					{schedule.assignedSessions === 0
+						? 'Brak przypisanej sesji!'
+						: `Używany w ${schedule.assignedSessions} ${
+								schedule.assignedSessions === 1 ? 'sesji' : 'sesjach'
+						  }`}
+				</p>
+			)}
+			{!!session && <p>{session.description}</p>}
+			<div className={styles.bottom}>
+				{schedule?.type && (
+					<p>
+						<Person /> {schedule.type === 'group' ? 'Grupowe' : '1:1'}
+					</p>
+				)}
+				{session?.price && (
+					<p>
+						<Money /> {session.price} zł
+					</p>
+				)}
+				{dateStart && dateEnd && (
+					<p>
+						<Calendar /> {dateStart.toLocaleDateString()} - {dateEnd.toLocaleDateString()}
+					</p>
+				)}
+				{time && (
+					<p>
+						<Timer />
+						{time} min
+					</p>
+				)}
+			</div>
+		</div>
+	);
+};
+
+export default ScheduleCard;
--- src/pages/app/Schedules/Schedules.tsx
+++ src/pages/app/Schedules/Schedules.tsx
@@ -7,17 +7,74 @@ import Button from '@newComponents/Button/Button';
 // Icons
 import Add from '@icons/Add';
 import Sessions from '@icons/Sessions';
+import Trash from './components/icons/Trash';
+import Pencil from './components/icons/Pencil';
 // Types
 import {Tag} from '@customTypes/tags';
 import {TitleTag, TitleVariant} from '@newComponents/typography/Title/Title';
 import {ButtonTag, ButtonVariant} from '@newComponents/Button/Button';
+import ScheduleCard, {
+	ScheduleCardProps,
+} from '@newComponents/Cards/ScheduleCard/ScheduleCard';
 // Styles
 import styles from './components/Empty/Empty.module.scss';
 import scheduleStyles from './Schedules.module.scss';
+import {Option} from '@newComponents/Cards/ScheduleCard/components/Options/Options';
 
 const SchedulesView = () => {
-	const [schedules, setSchedules] = useState([]);
-	const [sessions, setSessions] = useState([]);
+	const schedulesOptions = (id: string): Option[] => [
+		{
+			icon: <Pencil/>,
+			text: 'Edytuj',
+			onClick: () => {
+				console.log(id);
+			},
+		},
+		{
+			icon: <Trash/>,
+			text: 'Usuń',
+			onClick: () => {
+				console.log(id);
+			},
+		},
+	];
+	const sessionsOptions: Option[] = [];
+
+	const [schedules, setSchedules] = useState<ScheduleCardProps[]>([
+		{
+			id: '01',
+			title: 'Test',
+			dateStart: new Date(),
+			dateEnd: new Date(),
+			time: 60,
+			schedule: {type: 'individual', created: new Date(), assignedSessions: 1},
+			options: schedulesOptions('01'),
+		},
+		{
+			id: '02',
+			title: 'Test',
+			dateStart: new Date(),
+			dateEnd: new Date(),
+			time: 60,
+			schedule: {type: 'group', created: new Date(), assignedSessions: 0},
+			options: schedulesOptions('02'),
+		},
+	]);
+	const [sessions, setSessions] = useState<ScheduleCardProps[]>([
+		{
+			id: '01',
+			title: 'Test',
+			dateStart: new Date(),
+			dateEnd: new Date(),
+			time: 60,
+			options: sessionsOptions,
+			session: {
+				price: 250,
+				description:
+					'Figma ipsum component variant main layer. Arrange arrange reesizing selection ellipse. Union bold content distribute share fill variant rectangle. Duplicate editor device follower share. Union boolean overflow union align.',
+			},
+		},
+	]);
 
 	const currentView = useMemo(() => {
 		if (!!!schedules.length)
@@ -31,7 +88,7 @@ const SchedulesView = () => {
 
 		return (
 			<>
-				<Container as={Tag.Section}>
+				<Container as={Tag.Section} classes={scheduleStyles.container}>
 					<header className={scheduleStyles.header}>
 						<Title tag={TitleTag.h2} variant={TitleVariant.section}>
 							Harmonogramy
@@ -44,8 +101,13 @@ const SchedulesView = () => {
 							Dodaj <Add />
 						</Button>
 					</header>
+					<div>
+						{schedules.map((item) => (
+							<ScheduleCard {...item} />
+						))}
+					</div>
 				</Container>
-				<Container as={Tag.Section}>
+				<Container as={Tag.Section} classes={scheduleStyles.container}>
 					<header className={scheduleStyles.header}>
 						<Title tag={TitleTag.h2} variant={TitleVariant.section}>
 							Sesje
@@ -68,7 +130,11 @@ const SchedulesView = () => {
 							icon={<Sessions />}
 						/>
 					) : (
-						<></>
+						<div>
+							{sessions.map((item) => (
+								<ScheduleCard {...item} />
+							))}
+						</div>
 					)}
 				</Container>
 			</>

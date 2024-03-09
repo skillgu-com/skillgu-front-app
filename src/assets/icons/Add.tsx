2e390c28895d3aade4f6680448206ddb1bf3782d
Adrian Baran <74362768+AdrianBaranWeb@users.noreply.github.com>
2024-03-09 15:09:54 +0100

    Add empty UI

---
+++ src/assets/icons/Add.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Add = () => {
+	return (
+		<svg
+			width='24'
+			height='25'
+			viewBox='0 0 24 25'
+			fill='none'
+			xmlns='http://www.w3.org/2000/svg'>
+			<path
+				d='M6 12.4927H18'
+				stroke='#FF6A3D'
+				stroke-width='1.5'
+				stroke-linecap='round'
+				stroke-linejoin='round'
+			/>
+			<path
+				d='M12 18.4927V6.49268'
+				stroke='#FF6A3D'
+				stroke-width='1.5'
+				stroke-linecap='round'
+				stroke-linejoin='round'
+			/>
+		</svg>
+	);
+};
+
+export default Add;

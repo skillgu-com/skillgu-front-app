190b9cf21689559f4944945e18745ca1a0b0bd21
Adrian Baran <74362768+AdrianBaranWeb@users.noreply.github.com>
2024-04-09 23:53:42 +0200

    Change styles

--- src/new-components/Cards/ScheduleCard/ScheduleCard.tsx
+++ src/new-components/Cards/ScheduleCard/ScheduleCard.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useNavigate} from 'react-router-dom';
 // Components
 import Options from './components/Options/Options';
+import Modal from '@newComponents/Modal/Modal';
 // Icons
 import Money from './components/icons/Money';
 import Calendar from './components/icons/Calendar';
@@ -13,131 +14,163 @@ import Trash from 'src/pages/app/Schedules/components/icons/Trash';
 import styles from './ShceduleCard.module.scss';
 
 export interface ScheduleCardProps {
-    id: string;
-    dateStart: Date;
-    dateEnd: Date;
-    scheduleStartDay: string;
-    scheduleEndDay: string
-    meetTime: number;
-    title: string;
-    scheduleName: string;
-    sessionTypeName: string;
-    removeItem?: (id: string, arrayType: 'schedules' | 'sessions') => void;
-    assignedSession: number;
-    type: string;
-    participant: number;
-    sessionType: string;
-    description: string;
-    sessionPrice: number;
-    sessionName: string
+	id: string;
+	dateStart: Date;
+	dateEnd: Date;
+	scheduleStartDay: string;
+	scheduleEndDay: string;
+	meetTime: number;
+	title: string;
+	scheduleName: string;
+	sessionTypeName: string;
+	removeItem?: (id: string, arrayType: 'schedules' | 'sessions') => void;
+	assignedSession: number;
+	type: string;
+	participant: number;
+	sessionType: string;
+	description: string;
+	sessionPrice: number;
+	sessionName: string;
 
-    schedule?: {
-        type: 'individual' | 'group';
-        created: Date;
-        assignedSession: number;
-        scheduleName: string,
-        participant: number
-    };
-    session?: {
-        name: string;
-        description: string;
-        sessionPrice: number;
-        sessionName: string
-        meetTime: number
-        sessionTypeName: string;
-        scheduleName: string;
-    };
+	schedule?: {
+		type: 'individual' | 'group';
+		created: Date;
+		assignedSession: number;
+		scheduleName: string;
+		participant: number;
+	};
+	session?: {
+		name: string;
+		description: string;
+		sessionPrice: number;
+		sessionName: string;
+		meetTime: number;
+		sessionTypeName: string;
+		scheduleName: string;
+	};
 }
 
 const ScheduleCard = (props: ScheduleCardProps) => {
-    if (!!!props.schedule && !!!props.session)
-        throw new Error('One of parameters schedule or session is required!');
+	if (!!!props.schedule && !!!props.session)
+		throw new Error('One of parameters schedule or session is required!');
 
-    const {id, dateStart, dateEnd, meetTime, scheduleName, schedule, session, sessionTypeName, removeItem} = props;
+	const {
+		id,
+		dateStart,
+		dateEnd,
+		meetTime,
+		scheduleName,
+		schedule,
+		session,
+		sessionTypeName,
+		removeItem,
+	} = props;
 
+	const [openModal, setOpenModal] = useState(false);
 
-    const navigate = useNavigate();
+	const navigate = useNavigate();
 
-    const handleEditClick = () => {
-        console.log('Edycja')
-        navigate(!!session ? `/schedules/edit-session/${id}` : `/schedules/edit-schedule/${id}`);
-    };
+	const handleEditClick = () => {
+		console.log('Edycja');
+		navigate(
+			!!session
+				? `/schedules/edit-session/${id}`
+				: `/schedules/edit-schedule/${id}`
+		);
+	};
 
-    const handleDeleteClick = () => {
-        !!removeItem && removeItem(id, !!session ? 'sessions' : 'schedules');
-    };
+	const handleDeleteClick = () => {
+		!!removeItem && removeItem(id, !!session ? 'sessions' : 'schedules');
+	};
 
-    return (
-        <div className={styles.wrapper}>
-            <div className={styles.header}>
-                <h3 className={styles.title}>{schedule ? schedule?.scheduleName : session?.sessionTypeName}</h3>
-                <Options
-                    options={[
-                        {
-                            icon: <Pencil/>,
-                            text: 'Edytuj',
-                            onClick: handleEditClick,
-                        },
-                        {
-                            icon: <Trash/>,
-                            text: 'Usuń',
-                            onClick: handleDeleteClick,
-                        },
-                    ]}
-                />
-            </div>
-            {!!schedule && (
-                <>
-                    <p>Data stworzenia: {schedule.created.toLocaleDateString()}</p>
-                </>
-            )}
-            {!!schedule && (
-                <p data-is-assigned={schedule.assignedSession !== 0}>
-                    {schedule.assignedSession === 0
-                        ? 'Brak przypisanej sesji!'
-                        : `Używany w ${schedule.assignedSession} ${
-                            schedule.assignedSession === 1 ? 'sesji' : 'sesjach'
-                        }`}
-                </p>
-            )}
-            {!!session && (
-                <>
-                    <p>{session.description}</p>
-                </>
-            )}
+	return (
+		<>
+			<div className={styles.wrapper}>
+				<div className={styles.top}>
+					<div className={styles.header}>
+						<h3 className={styles.title}>
+							{schedule ? schedule?.scheduleName : session?.sessionName}
+						</h3>
 
-            <div className={styles.bottom}>
-                {schedule?.type && (
-                    <p>
-                        <Person/> {schedule.type === 'group' ? 'Grupowe' : '1:1'}
-                    </p>
-                )}
-                {session?.sessionPrice && (
-                    <p>
-                        <Money/> {session.sessionPrice} zł
-                    </p>
-                )}
-                {session && (
-                    <>
-                        {/*<p>{session.scheduleName}</p>*/}
-                        <p> Nazwa sesji: Nowe spotkania</p>
-                    </>
-                )}
+						<Options
+							options={[
+								{
+									icon: <Pencil />,
+									text: 'Edytuj',
+									onClick: handleEditClick,
+								},
+								{
+									icon: <Trash />,
+									text: 'Usuń',
+									onClick: handleDeleteClick,
+								},
+							]}
+						/>
+					</div>
+					{!schedule && <h4 className={styles.subtitle}>{session?.scheduleName}</h4>}
+					{!!schedule && (
+						<>
+							<p>Data stworzenia: {schedule.created.toLocaleDateString()}</p>
+						</>
+					)}
+					{!!schedule && (
+						<p data-is-assigned={schedule.assignedSession !== 0}>
+							{schedule.assignedSession === 0
+								? 'Brak przypisanej sesji!'
+								: `Używany w ${schedule.assignedSession} ${
+										schedule.assignedSession === 1 ? 'sesji' : 'sesjach'
+								  }`}
+						</p>
+					)}
+					{!!session && (
+						<>
+							<p>{session.description}</p>
+							<button
+								className={styles.more}
+								type='button'
+								onClick={() => {
+									setOpenModal(true);
+								}}>
+								Podgląd
+							</button>
+						</>
+					)}
+				</div>
 
-                {!session && dateStart && dateEnd && (
-                    <p>
-                        <Calendar/> {dateStart.toLocaleDateString()} - {dateEnd.toLocaleDateString()}
-                    </p>
-                )}
-                {meetTime && (
-                    <p>
-                        <Timer/>
-                        {meetTime} min
-                    </p>
-                )}
-            </div>
-        </div>
-    );
+				<div className={styles.bottom}>
+					{schedule?.type && (
+						<p>
+							<Person /> {schedule.type === 'group' ? 'Grupowe' : '1:1'}
+						</p>
+					)}
+					{session?.sessionPrice && (
+						<p>
+							<Money /> {session.sessionPrice} zł
+						</p>
+					)}
+					{!session && dateStart && dateEnd && (
+						<p>
+							<Calendar /> {dateStart.toLocaleDateString()} -{' '}
+							{dateEnd.toLocaleDateString()}
+						</p>
+					)}
+					{meetTime && (
+						<p>
+							<Timer />
+							{meetTime} min
+						</p>
+					)}
+				</div>
+			</div>
+			{!!session && openModal && (
+				<Modal
+					title={schedule ? schedule?.scheduleName : session?.sessionTypeName ?? ''}
+					closeHandler={() => setOpenModal(false)}>
+					<p className={styles.description}>{session?.description}</p>
+				</Modal>
+			)}
+		</>
+	);
 };
 
 export default ScheduleCard;

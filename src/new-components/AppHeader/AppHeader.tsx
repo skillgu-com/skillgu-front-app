241c49fb190068c93696a8e9387aa44fbbbeeca6
Radoslaw Zaczkiewicz <zaczkiewicz.radoslaw@gmail.com>
2024-04-13 01:38:44 +0100

    feat(ui): app layout and app header component redesign

--- src/new-components/AppHeader/AppHeader.tsx
+++ src/new-components/AppHeader/AppHeader.tsx
@@ -1,31 +1,37 @@
 // Libraires
-import React from 'react';
+import React from "react";
 // Components
-import Container from '../Container/Container';
-import {Title, Text} from '../typography';
+import Container from "../Container/Container";
+import { Title, Text } from "../typography";
 // Types
-import {TitleTag, TitleVariant} from '../typography/Title/Title';
-import {Tag} from 'src/types/tags';
+import { TitleTag, TitleVariant } from "../typography/Title/Title";
+import { Tag } from "src/types/tags";
 // Styles
-import styles from './AppHeader.module.scss';
-import {Common} from 'src/types/main';
+import styles from "./AppHeader.module.scss";
+import { Common } from "src/types/main";
 
 interface AppHeaderProps extends Common {
-	title: string;
-	text: string;
+  title: string;
+  text: string;
 }
 
 const AppHeader = (props: AppHeaderProps) => {
-	const {title, text} = props;
+  const { title, text } = props;
 
-	return (
-		<Container as={Tag.Header} classes={styles.header}>
-			<Title tag={TitleTag.h1} variant={TitleVariant.main}>
-				{title}
-			</Title>
-			<Text>{text}</Text>
-		</Container>
-	);
+  return (
+    <Container as={Tag.Header} classes={styles.header}>
+      <div className={styles.rows}>
+        <Title
+          classes={styles.title}
+          tag={TitleTag.h1}
+          variant={TitleVariant.main}
+        >
+          {title}
+        </Title>
+        <Text classes={styles.subtitle}>{text}</Text>
+      </div>
+    </Container>
+  );
 };
 
 export default AppHeader;

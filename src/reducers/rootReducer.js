bb7869586ba21788138a75bee1460788ce67aa47
Radoslaw Zaczkiewicz <zaczkiewicz.radoslaw@gmail.com>
2024-04-13 01:41:45 +0100

    feat(redux): update root reducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -5,7 +5,7 @@ import connectionProcessReducer from "./connectionProcessReducer";
 import userSettingReducer from "./userSettingReducer";
 import bookSessionReducer from "./bookSessionProcessReducer";
 import sessionIDReducer from "./calendarProcessReducer";
-
+import { termsReducer } from './terms';
 
 const rootReducer = combineReducers({
     auth: authReducer,
@@ -13,6 +13,7 @@ const rootReducer = combineReducers({
     userSetting: userSettingReducer,
     book: bookSessionReducer,
     sessionIDStep: sessionIDReducer,
+    terms: termsReducer,
     // Add other reducers if you have them
 });
 

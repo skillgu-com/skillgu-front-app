99635a06007fe402727df1542444df181e147cec
patrykrudzinski <patrykrudzinski@gmail.com>
2024-04-29 14:43:41 +0200

    login layout

--- src/new-components/_layouts/AuthLayout/AuthLayout.tsx
+++ src/new-components/_layouts/AuthLayout/AuthLayout.tsx
@@ -1,17 +1,22 @@
 import React, {FC, ReactNode} from "react";
+import {StyledContainer, StyledLogo, StyledCard} from "@newComponents/_layouts/AuthLayout/AuthLayout.styles";
+import {ReactComponent as SkillGuruLogo} from '@icons/svg/logo_skillguru-dark.svg';
+
 
 type Props = {
     children: ReactNode
 }
 
-const AuthLayout: FC<Props> = ({ children }) => {
+const AuthLayout: FC<Props> = ({children}) => {
 
 
     return (
-        <div>
-            AUTH LAYOUT
-            {children}
-        </div>
+        <StyledContainer>
+            <StyledLogo as={SkillGuruLogo}/>
+            <StyledCard>
+                {children}
+            </StyledCard>
+        </StyledContainer>
     );
 }
 

6e40443c6c1ff8bac4bc9ab093c080c81a848c7d
Radoslaw Zaczkiewicz <zaczkiewicz.radoslaw@gmail.com>
2024-05-04 16:05:04 +0100

    feat(mentor-profile): component Expandable

---
+++ src/new-components/_base/Expandable/Expandable.tsx
@@ -0,0 +1,44 @@
+import React, { ReactNode, useCallback, useEffect, useRef, useState } from "react";
+import AnimateHeight from "react-animate-height";
+import clx from "classnames";
+import styles from "./Expandable.module.scss";
+
+type Props = {
+  children?: ReactNode;
+  foldedHeight?: number | "auto";
+  isExpanded?: boolean;
+};
+
+export const Expandable = ({ children, foldedHeight = 0, isExpanded }: Props) => {
+  const [height, setHeight] = useState<number | "auto">(
+    foldedHeight
+  );
+  const started = useRef<boolean>(false)
+ 
+  useEffect(() => {
+    if(started.current && started.current === true){
+      setHeight((h) =>
+        h === foldedHeight ? "auto" : foldedHeight
+      )
+      return
+    }
+    started.current = true
+  }, [foldedHeight, isExpanded])
+
+  return (
+    <div>
+      <AnimateHeight
+        id="description"
+        duration={500}
+        height={height}
+        animateOpacity={true}
+        contentClassName={clx(styles.Content, {
+          [styles.ContentExpanded]:
+            height !== foldedHeight,
+        })}
+      >
+        {children}
+      </AnimateHeight>
+    </div>
+  );
+};

972d3a5e8091d6b3ff08c3150155dcbcca56b347
Radoslaw Zaczkiewicz <zaczkiewicz.radoslaw@gmail.com>
2024-06-09 09:52:39 +0100

    feat(chat): useChat reducer + page draft

--- src/pages/app/Chat/components/ChatContacts.tsx
+++ src/pages/app/Chat/components/ChatContacts.tsx
@@ -17,10 +17,96 @@ export const ChatContacts = () => {
   const initialPhraseLoad = useRef<string>("");
   useEffect(() => {
     const loadContacts = async () => {
-      const res = await fetch(
-        `http://localhost:3020/contacts?take=${PAGE_SIZE}`
-      );
-      const contacts = await res.json();
+      // const res = await fetch(
+      //   `http://localhost:3020/contacts?take=${PAGE_SIZE}`
+      // );
+      // const contacts = await res.json();
+      const contacts = [
+        {
+          id: 1,
+          fullName: "John Doe",
+          lastMessage: "Hello!",
+          avatarUrl: "",
+          unreadMessages: 1,
+        },
+        {
+          id: 2,
+          fullName: "Jane Smith",
+          lastMessage: "Hi there!",
+          avatarUrl: "",
+          unreadMessages: 0,
+        },
+        {
+          id: 4,
+          fullName: "Bob Brown",
+          lastMessage: "Good morning!",
+          avatarUrl: "",
+          unreadMessages: 0,
+        },
+        {
+          id: 5,
+          fullName: "John2 Doe",
+          lastMessage: "Hello!",
+          avatarUrl: "",
+          unreadMessages: 0,
+        },
+        {
+          id: 6,
+          fullName: "Jane2 Smith",
+          lastMessage: "Hi there!",
+          avatarUrl: "",
+          unreadMessages: 0,
+        },
+        {
+          id: 7,
+          fullName: "Bob2 Brown",
+          lastMessage: "Good morning!",
+          avatarUrl: "",
+          unreadMessages: 0,
+        },
+        {
+          id: 8,
+          fullName: "John3 Doe",
+          lastMessage: "Hello!",
+          avatarUrl: "",
+          unreadMessages: 0,
+        },
+        {
+          id: 9,
+          fullName: "Jane3 Smith",
+          lastMessage: "Hi there!",
+          avatarUrl: "",
+          unreadMessages: 0,
+        },
+        {
+          id: 10,
+          fullName: "Bob3 Brown",
+          lastMessage: "Good morning!",
+          avatarUrl: "",
+          unreadMessages: 0,
+        },
+        {
+          id: 11,
+          fullName: "John4 Doe",
+          lastMessage: "Hello!",
+          avatarUrl: "",
+          unreadMessages: 0,
+        },
+        {
+          id: 12,
+          fullName: "Jane4 Smith",
+          lastMessage: "Hi there!",
+          avatarUrl: "",
+          unreadMessages: 0,
+        },
+        {
+          id: 13,
+          fullName: "Bob4 Brown",
+          lastMessage: "Good morning!",
+          avatarUrl: "",
+          unreadMessages: 0,
+        },
+      ];
       setContacts(contacts, phrase);
     };
     if (initialPhraseLoad.current !== phrase) {
--- src/pages/app/Chat/components/ChatMessages.tsx
+++ src/pages/app/Chat/components/ChatMessages.tsx
@@ -12,12 +12,74 @@ export const ChatMessages = () => {
   useEffect(() => {
     const loadInitialMessages = async () => {
       setPending(true);
-      const res = await fetch(`http://localhost:3020/contacts/1/messages`);
-      const messages = await res.json();
+      // const res = await fetch(`http://localhost:3020/contacts/1/messages`);
+      // const messages = await res.json();
+      const messages = [
+        {
+          id: 1,
+          from: "John Doe",
+          text: "Cześć! Jak się masz?",
+          date: "2024-01-01 10:10",
+        },
+        {
+          id: 2,
+          from: "You",
+          text: "Cześć John! Wszystko w porządku, a Ty?",
+          date: "2024-01-01 10:20",
+        },
+        {
+          id: 3,
+          from: "John Doe",
+          text: "Mam się dobrze, dziękuję. Co u Ciebie słychać?",
+          date: "2024-01-01 10:30",
+        },
+        {
+          id: 4,
+          from: "You",
+          text: "Pracuję nad nowym projektem, jest bardzo interesujący.",
+          date: "2024-01-01 10:34",
+        },
+        {
+          id: 5,
+          from: "John Doe",
+          text: "To świetnie! Jakie są Twoje plany na weekend?",
+          date: "2024-01-01 10:40",
+        },
+        {
+          id: 6,
+          from: "You",
+          text: "Myślę o wyjeździe za miasto. A Ty?",
+          date: "2024-01-01 10:45",
+        },
+        {
+          id: 7,
+          from: "John Doe",
+          text: "Też mam takie plany. Może wybierzemy się razem?",
+          date: "2024-01-01 10:47",
+        },
+        {
+          id: 8,
+          from: "You",
+          text: "Brzmi świetnie! Gdzie chciałbyś pojechać?",
+          date: "2024-01-01 10:48",
+        },
+        {
+          id: 9,
+          from: "John Doe",
+          text: "Myślałem o górach. Lubisz wędrówki?",
+          date: "2024-01-01 10:50",
+        },
+        {
+          id: 10,
+          from: "You",
+          text: "Tak, bardzo! To będzie wspaniała przygoda.",
+          date: "2024-01-01 11:00",
+        },
+      ];
       addMessages(messages);
       setPending(false);
     };
-    if(chatState.messages.length === 0 && chatState.selectedContact){
+    if (chatState.messages.length === 0 && chatState.selectedContact) {
       loadInitialMessages();
     }
   }, [addMessages, chatState.selectedContact, chatState.messages]);

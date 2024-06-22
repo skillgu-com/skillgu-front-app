7c09f68002042adb2ce57c69743d2efca041d050
mkowack <kowackag@gmail.com>
2024-06-22 11:11:52 +0200

    feat: ChatContact

---
+++ src/pages/app/Chat/components/ChatContact/ChatContact.tsx
@@ -0,0 +1,32 @@
+import React, { MouseEventHandler } from "react";
+
+import styles from "./ChatContact.module.scss";
+import { ChatContactType } from "@customTypes/chat";
+import { Avatar } from "src/components/Avatar/Avatar";
+
+type ChatContactProps = {
+  contact: ChatContactType;
+  switchContact: MouseEventHandler<HTMLButtonElement>;
+};
+
+export const ChatContact = ({
+  contact: { id, avatarUrl, fullName, lastMessage, unreadMessages },
+  switchContact,
+}: ChatContactProps) => {
+  const generateShortMsg = (message: string) => {
+    return message.length > 25 ? `${message.slice(0, 25)}...` : message;
+  };
+
+  return (
+    <li key={id}>
+      <button onClick={switchContact} className={styles.button}>
+        <Avatar src={avatarUrl} alt={`${fullName} avatar`} />
+        <div>
+          <h4 className={styles.title}>{fullName}</h4>
+          <p className={styles.message}>{generateShortMsg(lastMessage)}</p>
+        </div>
+        {unreadMessages ?<div className={styles.unread}><p>{unreadMessages}</p></div> : null}
+      </button>
+    </li>
+  );
+};

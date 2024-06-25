a5ebfa2c61096534f727043917b4f1638c211d16
Radoslaw Zaczkiewicz <zaczkiewicz.radoslaw@gmail.com>
2024-06-25 01:19:29 +0100

    feat: payments: faq, box border, navbar config

--- src/assets/icons/DollarCircleIcon.tsx
+++ src/assets/icons/DollarCircleIcon.tsx
@@ -25,14 +25,14 @@ export const DollarCircleIcon = ({
         stroke={color}
         strokeLinecap="round"
         strokeLinejoin="round"
-        strokeWidth="1.5"
+        strokeWidth="1.0"
         d="M5.781 9.553c0 .86.66 1.553 1.48 1.553h1.674c.713 0 1.293-.606 1.293-1.353 0-.813-.353-1.1-.88-1.287L6.66 7.533c-.526-.187-.88-.473-.88-1.287 0-.746.58-1.353 1.294-1.353h1.673c.82 0 1.48.693 1.48 1.553M8 4v8"
       ></path>
       <path
         stroke={color}
         strokeLinecap="round"
         strokeLinejoin="round"
-        strokeWidth="1.5"
+        strokeWidth="1.0"
         d="M8 14.667A6.667 6.667 0 108 1.334a6.667 6.667 0 000 13.333z"
       ></path>
     </svg>
---
+++ src/components/AppLayout/components/Navbar/config.tsx
@@ -0,0 +1,102 @@
+// Libraries
+import React, { ReactNode } from "react";
+import Doc from "../../../../assets/icons/Doc";
+import Home from "../../../../assets/icons/Home";
+import Settings from "../../../../assets/icons/Settings";
+import CreateSchedules from "../../../../assets/icons/CreateSchedules";
+import { ReactComponent as CalendarIcon } from "../../../../assets/icons/svg/calendar.svg";
+import paths from "../../../../paths";
+import { DollarCircleIcon } from "@icons/DollarCircleIcon";
+
+export type Visibility = "student" | "mentor" | "authenticated" | "all";
+
+export type MenuItemType = {
+  id: string;
+  label: string;
+  link: string;
+  icon: ReactNode;
+  visibility: Visibility;
+};
+
+type Props = {
+  username: string;
+  role: "M" | "S" | "";
+};
+
+export const getMenuItems = ({ username, role }: Props): MenuItemType[] => {
+  return [
+    {
+      id: "home",
+      label: "Strona główna",
+      link: "/home",
+      icon: <Home />,
+      visibility: "all",
+    },
+    {
+      id: "calendar",
+      label: "Kalendarz",
+      link: paths.calendar,
+      icon: <CalendarIcon />,
+      visibility: "authenticated",
+    },
+    {
+      id: "profile",
+      label: "Profil",
+      link: `/student/${username}`,
+      icon: <Doc />,
+      visibility: "student",
+    },
+    {
+      id: "profile",
+      label: "Profil",
+      link: `/mentor/$username`,
+      icon: <Doc />,
+      visibility: "mentor",
+    },
+    {
+      id: "schedules",
+      label: "Tworzenie spotkań",
+      link: "/schedules",
+      icon: <CreateSchedules />,
+      visibility: "mentor",
+    },
+    {
+      id: "settings",
+      label: "Ustawienia",
+      link: `/edit-mentor/${username}`,
+      icon: <Settings />,
+      visibility: "mentor",
+    },
+    {
+      id: "settings",
+      label: "Ustawienia",
+      link: `/edit-student/${username}`,
+      icon: <Settings />,
+      visibility: "student",
+    },
+    {
+      id: "payments",
+      label: "Płatności",
+      link: `/payment`,
+      icon: <DollarCircleIcon />,
+      visibility: "mentor",
+    },
+    {
+      id: "payments",
+      label: "Płatności",
+      link: `/payment/${username}`,
+      icon: <DollarCircleIcon />,
+      visibility: "student",
+    },
+  ].filter((item) => {
+    if (
+      item.visibility === "all" ||
+      (item.visibility === "authenticated" && !!role) ||
+      (item.visibility === "mentor" && role === "M") ||
+      (item.visibility === "student" && role === "S")
+    ) {
+      return true;
+    }
+    return false;
+  }) as MenuItemType[];
+};

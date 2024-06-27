8d9dd37dbf5db58d799b30b7267a260100384a45
maciejmaciek <hello@maciejglazer.com>
2024-06-27 07:01:26 +0200

    Revert "Merge remote-tracking branch 'origin/feat/payment-new-features' into feature-faq-platnosci-mg"
    This reverts commit d90d8361a969cb506a45db20f9cd87fc280e69de, reversing
    changes made to 928c873cfd46346c834aa1586d6669d5bda6731e.

--- src/pages/app/MentorPaymentIntegration/elements/Reports.tsx
+++ src/pages/app/MentorPaymentIntegration/elements/Reports.tsx
@@ -5,6 +5,12 @@ import { formatDate } from "src/utils";
 import { formatPrice } from "src/utils/price";
 import { ReportStatus, Report } from "@customTypes/reports";
 import { Status } from "src/components/_base/Status";
+import styles from "./styles.module.scss";
+import { Scrollable } from "src/components/_base/Scrollable";
+import { PdfIcon } from "@icons/PdfIcon";
+import { Loader } from "src/components/_grouped/loader";
+import { fetchPaymentReports } from "@services/payments/fetchPaymentReports.service";
+import { FetchPaymentReportsServiceOutput } from "@services/payments/fetchPaymentReports.types";
 
 const renderStatus = (status: ReportStatus) => {
   switch (status) {
@@ -19,63 +25,92 @@ const renderStatus = (status: ReportStatus) => {
   }
 };
 
+const PER_PAGE = 8;
+
 export const Reports = () => {
-  const [reports, setReports] = useState<Report[]>([]);
+  const [data, setData] = useState<FetchPaymentReportsServiceOutput | null>(
+    null
+  );
+  const [pending, setPending] = useState<boolean>(true);
+  const [page, setPage] = useState<number>(1);
 
   useEffect(() => {
-    const run = async () => {
-      const res = await fetch("/mocked-reports.json");
-      const data = await res.json();
-      setReports(data as Report[]);
+    const run = async (page: number) => {
+      setPending(true);
+      const data = await fetchPaymentReports({
+        take: PER_PAGE,
+        skip: PER_PAGE * (page - 1),
+      });
+      setData(data);
+      setPending(false);
     };
-    run();
-  }, []);
+    run(page);
+  }, [page]);
 
-  return (
+  return pending ? (
+    <Loader spinner spinnerSize="lg" shadow overflow />
+  ) : data === null ? (
+    <div className={styles.alert}>
+      <p>Brak danych</p>
+    </div>
+  ) : (data && data.success && data.reports.length) ? (
     <section>
       <Table>
-        <TableRow heading>
-          <TableCell flex={1} heading text="Faktura" />
-          <TableCell flex={1} heading text="Data" />
-          <TableCell flex={1} heading text="Kwota" />
-          <TableCell flex={2} heading text="Status" />
-          <TableCell flex={3} heading text="Sesja" />
-          <TableCell flex={3} heading />
-        </TableRow>
+        <Scrollable minWidth={"920px"}>
+          <TableRow heading>
+            <TableCell width="100px" heading text="Faktura" />
+            <TableCell flex={1} heading text="Data" />
+            <TableCell flex={1} heading text="Kwota" />
+            <TableCell width="167px" heading text="Status" />
+            <TableCell flex={3} heading text="Sesja" />
+            <TableCell flex={3} heading />
+          </TableRow>
 
-        {reports
-          ? reports.map((r) => (
-              <TableRow key={r.id}>
-                <TableCell flex={1}>#{r.invoiceNo}</TableCell>
-                <TableCell flex={1}>
-                  {formatDate(r.date, "DD MMMM YYYY")}
-                </TableCell>
-                <TableCell flex={1}>
-                  {formatPrice(r.amount, { minimumFractionDigits: 0 })}
-                </TableCell>
-                <TableCell flex={2}>{renderStatus(r.status)}</TableCell>
-                <TableCell flex={3}>{r.sessionTitle}</TableCell>
-                <TableCell flex={3}>
-                  <a href={r.invoiceFileUrl} download={r.invoiceNo}>
-                    Pobierz fakturę PDF
-                  </a>
-                </TableCell>
-              </TableRow>
-            ))
-          : null}
-
+          {data.reports
+            ? data.reports.map((r) => (
+                <TableRow>
+                  <TableCell width="100px">{r.invoiceNo}</TableCell>
+                  <TableCell flex={1}>
+                    {formatDate(r.date, "DD.MM.YYYY")}
+                  </TableCell>
+                  <TableCell flex={1}>
+                    {formatPrice(r.amount, { minimumFractionDigits: 0 })}
+                  </TableCell>
+                  <TableCell width="167px">{renderStatus(r.status)}</TableCell>
+                  <TableCell flex={3}>{r.sessionTitle}</TableCell>
+                  <TableCell flex={3}>
+                    <a
+                      href={r.invoiceFileUrl}
+                      download={r.invoiceNo}
+                      className={styles.pdfBtn}
+                    >
+                      Pobierz fakturę <PdfIcon />
+                    </a>
+                  </TableCell>
+                </TableRow>
+              ))
+            : null}
+        </Scrollable>
         <TableRow heading>
           <TableCell flex /*width="100%"*/>
             <Pagination
               name="reports-pagination"
-              current={2}
-              last={10}
-              onClick={() => {}}
-              // className={styles.pagination}
+              current={page}
+              last={Math.ceil(data.total / PER_PAGE)}
+              onClick={(e) => {
+                const btn = e.target as HTMLButtonElement;
+                setPage(Number(btn.value));
+              }}
+              className={styles.pagination}
+              fullWidth
             />
           </TableCell>
         </TableRow>
       </Table>
     </section>
+  ) : (
+    <div className={styles.alert}>
+      <p>Brak raportów</p>
+    </div>
   );
 };
--- src/pages/app/MentorPaymentIntegration/screens/Connected.tsx
+++ src/pages/app/MentorPaymentIntegration/screens/Connected.tsx
@@ -1,16 +1,11 @@
-import React from "react";
-import clx from "classnames";
-
-import Button, { ButtonVariant } from "src/components/Button/Button";
-import { payment } from "src/components/FAQ/Accordion/content/payment";
-import FAQ from "src/components/FAQ/Accordion/Accordion";
-import Container from "src/components/Container/Container";
+import React, { useEffect, useState } from "react";
 import { LinksList, LinksListRow } from "src/components/FAQ/LinksList";
-import { Reports } from "../elements/Reports";
-import { Schedule } from "../elements/Schedule";
-
+import clx from "classnames";
 import styles from "../styles.module.scss";
-import { Tag } from "@customTypes/tags";
+import Button, { ButtonVariant } from "src/components/Button/Button";
+import { payment } from "../../../../components/FAQ/Accordion/content/payment";
+import FAQ from "src/components/FAQ/Accordion/Accordion";
+import { Reports } from "../elements/Reports";
 
 type Props = {
   price: number;
@@ -27,49 +22,43 @@ export const Connected = ({ price, error, handleCreateAccountLink }: Props) => {
     : "";
 
   return (
-    <main>
-      <Container as={Tag.Section} classes={styles.wrapper}>
-        <h1 className={styles.title}>Płatności</h1>
-        <p className={styles.description}>
-          Płatności na platformie obsługuje firma Stripe. Jeżeli chcesz edytować
-          jakieś dane lub wypłacić środki, kliknij "Przejdź do Stripe".
-        </p>
-        <div className={clx(styles.cols2)}>
-          <div className={styles.priceCtaWrapper}>
-            <div className={styles.priceCtaBox}>
-              {formattedPrice ? (
-                <span className={styles.priceLabel}>
-                  Obecne saldo wynosi
-                  <span className={styles.priceAmount}>
-                    {formattedPrice} zł
-                  </span>
-                </span>
-              ) : null}
-              <div>
-                <Button
-                  onClick={handleCreateAccountLink}
-                  fullWidth
-                  variant={ButtonVariant.Primary}
-                >
-                  Połącz ze Stripe
-                </Button>
-                {error && <p>Error occurred while processing your request.</p>}
-              </div>
+    <div className={styles.wrapper}>
+      <h1 className={styles.title}>Płatności</h1>
+      <p className={styles.description}>
+        Płatności na platformie obsługuje firma Stripe. Jeżeli chcesz edytować
+        jakieś dane lub wypłacić środki, kliknij "Przejdź do Stripe".
+      </p>
+      <div className={clx(styles.cols)}>
+        <div className={styles.priceCtaWrapper}>
+          <div className={styles.priceCtaBox}>
+            {formattedPrice ? (
+              <span className={styles.priceLabel}>
+                Obecne saldo wynosi
+                <span className={styles.priceAmount}>{formattedPrice} zł</span>
+              </span>
+            ) : null}
+            <div>
+              <Button
+                onClick={handleCreateAccountLink}
+                fullWidth
+                variant={ButtonVariant.Primary}
+              >
+                Połącz ze Stripe
+              </Button>
+              {error && <p>Error occurred while processing your request.</p>}
             </div>
           </div>
-          <div className={styles.scheduleWrapper}>
-            <Schedule />
-          </div>
         </div>
-        <section>
-          <Reports />
-        </section>
-        <section>
-          <div className={styles.faqBox}>
-            <FAQ title="FAQ" elements={payment} />
-          </div>
-        </section>
-      </Container>
-    </main>
+        <div className={styles.faqWrapper}></div>
+      </div>
+      <section>
+        <Reports />
+      </section>
+      <section>
+        <div className={styles.faqBox}>
+          <FAQ title="FAQ" elements={payment} />
+        </div>
+      </section>
+    </div>
   );
 };

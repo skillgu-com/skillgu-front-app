9cb927b24b54b5e532c6ac24fd83c57fe0e99e9e
maciejmaciek <hello@maciejglazer.com>
2024-06-27 15:10:06 +0200

    update

--- src/services/mentor/fetchSimilarMentors.service.ts
+++ src/services/mentor/fetchSimilarMentors.service.ts
@@ -2,14 +2,32 @@ import {
   FetchSimilarMentorsOutput,
   FetchSimilarMentorsInput,
 } from "./fetchSimilarMentors.types";
+import axios from "axios";
 
-export const fetchSimilarMentors = async (
-  props: FetchSimilarMentorsInput
-): Promise<FetchSimilarMentorsOutput> => {
-  const res = await fetch('/similar-mentors.json')
-  const data = await res.json() 
+// export const fetchSimilarMentors = async (
+//   props: FetchSimilarMentorsInput
+// ): Promise<FetchSimilarMentorsOutput> => {
+//   const res = await fetch('/similar-mentors.json')
+//   const data = await res.json()
+//
+//   return {
+//     mentors: data.slice(0, props.take),
+//   };
+// };
 
-  return {
-    mentors: data.slice(0, props.take),
-  };
+
+export const fetchSimilarMentors = async (props: FetchSimilarMentorsInput): Promise<FetchSimilarMentorsOutput> => {
+  try {
+    const response = await axios.get('/mentor/home/mentors/similar');
+    const data = response.data;
+
+    console.log('similar mentors are: TEST ', data)
+
+    return {
+      mentors: data.slice(0, props.take),
+    };
+  } catch (error) {
+    console.error('Error fetching similar mentors:', error);
+    throw error;
+  }
 };

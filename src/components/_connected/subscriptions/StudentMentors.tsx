c1c69b357f03aafb7a3abccf8efbe076a6028f55
Radoslaw Zaczkiewicz <zaczkiewicz.radoslaw@gmail.com>
2024-07-02 09:19:14 +0100

    feat: student suspend/cance/feedback mentorship

--- src/components/_connected/subscriptions/StudentMentors.tsx
+++ src/components/_connected/subscriptions/StudentMentors.tsx
@@ -1,23 +1,53 @@
-import React, {useCallback, useEffect, useRef, useState,} from "react";
+import React, {
+  ReactNode,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import styles from "./Subscriptions.module.scss";
-import {SubscriptionStatus} from "@customTypes/subscriptions";
-import {UserIdentity} from "src/components/_base/UserIdentity";
-import {Status} from "src/components/_base/Status";
-import {Tag} from "src/types/tags";
+import {
+  HorizontalTabs,
+  HorizontalTabsButton,
+} from "src/components/_base/HorizontalTabs";
+import { SubscriptionStatus } from "@customTypes/subscriptions";
+import { Table, TableCell, TableRow } from "src/components/_base/Table";
+import { Scrollable } from "src/components/_base/Scrollable";
+import { Pagination } from "src/components/_grouped";
+import { formatDate } from "src/utils";
+import { UserIdentity } from "src/components/_base/UserIdentity";
+import { CrownIcon } from "@icons/CrownIcon";
+import { Status } from "src/components/_base/Status";
+import { Tag } from "src/types/tags";
 import Container from "src/components/Container/Container";
-import {Skeleton, Typography} from "@mui/material";
+import { SearchSvg2 } from "@icons/SearchSvg2";
+import { SkeletonRow } from "./SkeletonRow";
+import {
+  OverflowMenu,
+  OverflowMenuList,
+  OverflowMenuOption,
+  OverflowMenuToggle,
+} from "src/components/_grouped/overflow-menu";
+import { useNavigate } from "react-router-dom";
+import { useSubscriptionsReducer } from "src/reducers/subscriptions";
+import { Skeleton, Typography } from "@mui/material";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import {PlanName} from "src/components/_base/PlanName";
-import {fetchStudentMentors} from "@services/mentee/fetchStudentMentors.service";
-import {FetchStudentMentorsOutput} from "@services/mentee/fetchStudentMentors.types";
-import {ArrowLongRight} from "@icons/ArrowLongRight";
+import { PlanName } from "src/components/_base/PlanName";
+import SendArrow from "@icons/SendArrow";
+import { fetchStudentMentors } from "@services/mentee/fetchStudentMentors.service";
+import { FetchStudentMentorsOutput } from "@services/mentee/fetchStudentMentors.types";
+import Button from "src/components/Button/Button";
+import Arrow from "@icons/Arrow";
+import { ArrowLongRight } from "@icons/ArrowLongRight";
 import clx from "classnames";
-import {SubscriptionPlan} from "@customTypes/order";
-import {cancelMentorship} from "@services/mentorship/cancelMentorship";
-import {suspendMentorship} from "@services/mentorship/suspendMentorship";
-
+import { ClientPortal } from "src/components/portal";
+import { SubscriptionPlan } from "@customTypes/order";
+import Modal from "src/components/Modal/Modal";
+import { MentorshipFeedbackModal } from "../mentorship-feedback/MentorshipFeedbackModal";
+import { cancelMentorship } from "@services/mentorship/cancelMentorship";
+import { suspendMentorship } from "@services/mentorship/suspendMentorship";
 
 const PER_PAGE = 5;
 
@@ -89,7 +119,6 @@ export const StudentMentors = ({ title }: Props) => {
   const [cancelling, setCancelling] = useState<MentorShort | null>(null);
   const [cancelled, setCancelled] = useState<MentorShort | null>(null);
 
-
   useEffect(() => {
     const fetchData = async () => {
       const data = await fetchStudentMentors({
@@ -121,133 +150,217 @@ export const StudentMentors = ({ title }: Props) => {
   }, [suspending]);
 
   return (
-    <Container as={Tag.Section}>
-      <div className={styles.wrapper}>
-        <div className={styles.header}>
-          <h2>{title}</h2>
-        </div>
+    <>
+      <ClientPortal selector="modal-root">
+        {cancelling ? (
+          <Modal title="" closeHandler={() => setCancelling(null)}>
+            <div>
+              <h2>
+                Czy jesteś pewny, że chcesz anulować swój plan z{" "}
+                {cancelling.fullName}?
+              </h2>
+              <p>
+                Dostęp do funkcji premium będziesz miał jeszcze do{" "}
+                {formatDate(cancelling.paidUntil, "DD MMMM YYYY")} roku.
+              </p>
+            </div>
+            <button onClick={handleCancelConfirm}>
+              Tak, zakończ subskrypcję
+            </button>
+            <button onClick={() => setCancelling(null)}>
+              Nie, jeszcze nie
+            </button>
+          </Modal>
+        ) : null}
+        {suspending ? (
+          <Modal title="" closeHandler={() => setSuspending(null)}>
+            <div>
+              <h2>
+                Czy jesteś pewny, że chcesz zawiesić swój plan z{" "}
+                {suspending.fullName}?
+              </h2>
+              <p>
+                Dostęp do funkcji premium będziesz miał jeszcze do{" "}
+                {formatDate(suspending.paidUntil, "DD MMMM YYYY")} roku.
+              </p>
+            </div>
+            <button onClick={handleSuspendConfirm}>
+              Tak, zawieś subskrypcję
+            </button>
+            <button onClick={() => setSuspending(null)}>
+              Nie, jeszcze nie
+            </button>
+          </Modal>
+        ) : null}
+        {cancelled ? (
+          <MentorshipFeedbackModal
+            mentorshipId={cancelled.id}
+            handleClose={() => setCancelled(null)}
+          />
+        ) : null}
+      </ClientPortal>
 
-        <div className={styles.body}>
-          {pending ? (
-            <Slider {...settings} className={styles.slick}>
-              {new Array(PER_PAGE).fill(null).map((_, i) => (
-                <div key={i} className={styles.slickItem}>
-                  <div className={styles.card}>
-                    <UserIdentity
-                      avatar={
-                        <Skeleton
-                          className={styles.img}
-                          variant="circular"
-                          width={56}
-                          height={56}
-                        />
-                      }
-                      title={
-                        <Skeleton style={{ width: "90%" }} variant="text" />
-                      }
-                      subtitle={
-                        <Skeleton style={{ width: "90%" }} variant="text" />
-                      }
-                    />
-                  </div>
-                </div>
-              ))}
-            </Slider>
-          ) : null}
-          {!pending &&
-          data &&
-          data?.total &&
-          data.mentors &&
-          data.mentors.length > 0 ? (
-            <Slider {...settings} className={styles.slick}>
-              {data.mentors.map((m) => (
-                <div key={m.id} className={styles.studentMentorSlickItem}>
-                  <div className={styles.studentMentorCard}>
-                    <div>
+      <Container as={Tag.Section}>
+        <div className={styles.wrapper}>
+          <div className={styles.header}>
+            <h2>{title}</h2>
+          </div>
+
+          <div className={styles.body}>
+            {pending ? (
+              <Slider {...settings} className={styles.slick}>
+                {new Array(PER_PAGE).fill(null).map((_, i) => (
+                  <div key={i} className={styles.slickItem}>
+                    <div className={styles.card}>
                       <UserIdentity
-                        className={styles.userIdentity}
-                        avatarSize={56}
-                        avatarAlt={m.fullName}
-                        avatarUrl={m.avatarUrl}
+                        avatar={
+                          <Skeleton
+                            className={styles.img}
+                            variant="circular"
+                            width={56}
+                            height={56}
+                          />
+                        }
                         title={
-                          <Typography variant="buttonLg" color="secondary">
-                            {m.fullName}
-                          </Typography>
+                          <Skeleton style={{ width: "90%" }} variant="text" />
                         }
                         subtitle={
-                          <a
-                            className={styles.profileLink}
-                            href={`/mentor/${m.id}`}
-                          >
-                            Zobacz profil
-                            <ArrowLongRight />
-                          </a>
+                          <Skeleton style={{ width: "90%" }} variant="text" />
                         }
                       />
-                      <PlanName
-                        plan={m.plan}
-                        pillBg
-                        className={styles.planPill}
-                      />
                     </div>
-                    <div className={styles.hr} />
-                    {m.status === "awaiting" ? (
-                      <>
-                        <div className={styles.cardStatus}>
-                          <Status text="Aplikacja w toku" variant="warning" />
-                          <p>
-                            Jeżeli zostanie zaakceptowana, poprosimy Cię o
-                            wybranie terminów.
-                          </p>
-                        </div>
-                        <div className={styles.buttons}>
-                          <button className={styles.btn}>Zobacz aplikację</button>
-                        </div>
-                      </>
-                    ) : null}
-                    {m.status === "rejected" ? (
-                      <>
-                        <div className={styles.cardStatus}>
-                          <Status text="Aplikacja odrzucona" variant="danger" />
-                          <p>Mentor odrzucił Twoją aplikację.</p>
-                        </div>
-                        <div className={styles.buttons}>
-                          <button className={styles.btn}>Zobacz powód odrzucenia</button>
-                        </div>
-                      </>
-                    ) : null}
-                    {m.status === "accepted" && !m.scheduled ? (
-                      <>
-                        <div className={styles.cardStatus}>
-                          <Status
-                            text="Aplikacja zaakceptowana"
-                            variant="success"
-                          />
-                          <p>
-                            Jeżeli zostanie zaakceptowana, poprosimy Cię o
-                            wybranie terminów.
-                          </p>
-                        </div>
-                        <div className={styles.buttons}>
-                          <button className={styles.btn}>Wybierz terminy spotkań</button>
-                        </div>
-                      </>
-                    ) : null}
-                    {m.status === "accepted" && m.scheduled ? (
-                      <>
-                        <div className={styles.buttons}>
-                          <button className={styles.btn}>Zawieś subskrypcję</button>
-                          <button className={clx(styles.btn, styles.btnRed)}>Zakończ subskrypcję</button>
-                        </div>
-                      </>
-                    ) : null}
                   </div>
-                </div>
-              ))}
-            </Slider>
-          ) : null}
+                ))}
+              </Slider>
+            ) : null}
+            {!pending &&
+            data &&
+            data?.total &&
+            data.mentors &&
+            data.mentors.length > 0 ? (
+              <Slider {...settings} className={styles.slick}>
+                {data.mentors.map((m) => (
+                  <div key={m.id} className={styles.studentMentorSlickItem}>
+                    <div className={styles.studentMentorCard}>
+                      <div>
+                        <UserIdentity
+                          className={styles.userIdentity}
+                          avatarSize={56}
+                          avatarAlt={m.fullName}
+                          avatarUrl={m.avatarUrl}
+                          title={
+                            <Typography variant="buttonLg" color="secondary">
+                              {m.fullName}
+                            </Typography>
+                          }
+                          subtitle={
+                            <a
+                              className={styles.profileLink}
+                              href={`/mentor/${m.id}`}
+                            >
+                              Zobacz profil
+                              <ArrowLongRight />
+                            </a>
+                          }
+                        />
+                        <PlanName
+                          plan={m.plan}
+                          pillBg
+                          className={styles.planPill}
+                        />
+                      </div>
+                      <div className={styles.hr} />
+                      {m.status === "awaiting" ? (
+                        <>
+                          <div className={styles.cardStatus}>
+                            <Status text="Aplikacja w toku" variant="warning" />
+                            <p>
+                              Jeżeli zostanie zaakceptowana, poprosimy Cię o
+                              wybranie terminów.
+                            </p>
+                          </div>
+                          <div className={styles.buttons}>
+                            <button className={styles.btn}>
+                              Zobacz aplikację
+                            </button>
+                          </div>
+                        </>
+                      ) : null}
+                      {m.status === "rejected" ? (
+                        <>
+                          <div className={styles.cardStatus}>
+                            <Status
+                              text="Aplikacja odrzucona"
+                              variant="danger"
+                            />
+                            <p>Mentor odrzucił Twoją aplikację.</p>
+                          </div>
+                          <div className={styles.buttons}>
+                            <button className={styles.btn}>
+                              Zobacz powód odrzucenia
+                            </button>
+                          </div>
+                        </>
+                      ) : null}
+                      {m.status === "accepted" && !m.scheduled ? (
+                        <>
+                          <div className={styles.cardStatus}>
+                            <Status
+                              text="Aplikacja zaakceptowana"
+                              variant="success"
+                            />
+                            <p>
+                              Jeżeli zostanie zaakceptowana, poprosimy Cię o
+                              wybranie terminów.
+                            </p>
+                          </div>
+                          <div className={styles.buttons}>
+                            <button className={styles.btn}>
+                              Wybierz terminy spotkań
+                            </button>
+                          </div>
+                        </>
+                      ) : null}
+                      {m.status === "accepted" && m.scheduled ? (
+                        <>
+                          <div className={styles.buttons}>
+                            <button
+                              onClick={() =>
+                                setSuspending({
+                                  id: m.id,
+                                  fullName: m.fullName,
+                                  plan: m.plan,
+                                  paidUntil: m.paidUntil,
+                                })
+                              }
+                              className={styles.btn}
+                            >
+                              Zawieś subskrypcję
+                            </button>
+                            <button
+                              onClick={() =>
+                                setCancelling({
+                                  id: m.id,
+                                  fullName: m.fullName,
+                                  plan: m.plan,
+                                  paidUntil: m.paidUntil,
+                                })
+                              }
+                              className={clx(styles.btn, styles.btnRed)}
+                            >
+                              Zakończ subskrypcję
+                            </button>
+                          </div>
+                        </>
+                      ) : null}
+                    </div>
+                  </div>
+                ))}
+              </Slider>
+            ) : null}
+          </div>
         </div>
-      </div>
-    </Container>
+      </Container>
+    </>
   );
 };

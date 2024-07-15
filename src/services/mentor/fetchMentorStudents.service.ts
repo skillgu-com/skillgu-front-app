a87877f5ad735a623abca28011977bb6fb242f6c
maciejmaciek <hello@maciejglazer.com>
2024-07-15 14:17:59 +0200

    update

--- src/services/mentor/fetchMentorStudents.service.ts
+++ src/services/mentor/fetchMentorStudents.service.ts
@@ -6,8 +6,7 @@ import {
 export const fetchMentorStudents = async (
   props: FetchMentorStudentsInput
 ): Promise<FetchMentorStudentsOutput> => {
-  const res = await fetch('/mentor-students.json')
-  console.log('pizda')
+  const res = await fetch('/mentor-studentss.json')
   const data = await res.json() as FetchMentorStudentsOutput['students']
   const filtered = data.filter(d => d.status === props.status)
 

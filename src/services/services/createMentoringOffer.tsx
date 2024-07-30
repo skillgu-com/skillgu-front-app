6e565f479033d2210c83331d19e5295ebfce977e
macglazer <hello@maciejglazer.com>
2024-07-30 18:17:41 +0200

    update

--- src/services/services/createMentoringOffer.tsx
+++ src/services/services/createMentoringOffer.tsx
@@ -27,7 +27,7 @@ export const createMentoringOffer = async (input: Input): Promise<Response> => {
     };
     console.log('body:', body)
     try {
-        const response = await axios.post('/api/mentorship/create/mentorship-plansTEST', body);
+        const response = await axios.post('/api/mentorship/create/mentorship-plans', body);
         if (response.status === 200) {
             return {success: true};
         } else {

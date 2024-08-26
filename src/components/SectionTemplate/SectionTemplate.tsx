e8c221805e46097c89969441af82efc77ca31506
mkowack <kowackag@gmail.com>
2024-08-26 15:08:04 +0200

    feat:create SectionTemplate

---
+++ src/components/SectionTemplate/SectionTemplate.tsx
@@ -0,0 +1,49 @@
+import React, { ReactNode } from "react";
+import clx from "classnames";
+
+import Container from "src/components/Container/Container";
+import { TitleTag, TitleVariant } from "src/components/typography/Title/Title";
+import { Title } from "src/components/typography";
+
+import { Tag } from "@customTypes/tags";
+
+import styles from "./SectionTemplate.module.scss";
+
+type SectionTemplatePropsType = {
+  title: string;
+  additionalContent?: ReactNode;
+  description?: ReactNode;
+  children: ReactNode;
+  className?: string;
+  additionalClassName?: string;
+};
+
+export const SectionTemplate = ({
+  title,
+  additionalContent,
+  description,
+  children,
+  className,
+  additionalClassName,
+}: SectionTemplatePropsType) => {
+  return (
+    <Container as={Tag.Section} classes={clx(styles.container, className)}>
+      <header className={styles.header}>
+        <div className={clx(styles.titleWrapper, additionalClassName)}>
+          <Title
+            tag={TitleTag.h2}
+            variant={TitleVariant.section}
+            classes={styles.title}
+          >
+            {title}
+          </Title>
+          {additionalContent}
+        </div>
+        {description ? (
+          <p className={styles.description}>{description}</p>
+        ) : null}
+      </header>
+      {children}
+    </Container>
+  );
+};

6dac2d0713bf12171a93a76bcfe1f030c9a0a59d
mkowack <kowackag@gmail.com>
2024-09-05 23:05:40 +0200

    feat: finish Pricing section

--- src/components/_connected/pricing/Pricing.tsx
+++ src/components/_connected/pricing/Pricing.tsx
@@ -1,159 +1,60 @@
-import React, { ReactNode, useState } from "react";
+import React from "react";
 import styles from "./Pricing.module.scss";
-import { SectionTemplate } from "src/components/SectionTemplate";
 import { Pro } from "@icons/Pro";
 import { Basic } from "@icons/Basic";
 import { CheckCircle } from "@icons/CheckCircle";
 
-type Props = {
-  title?: string;
-  subtitle?: ReactNode;
+type PricingPropsType = {
+  planTitle: string;
+  price: number;
+  values: string[];
+  selectedPlan: string;
+  handleSelectPlan: (plan: string) => void;
 };
 
-export const Pricing = ({ title = "Twoje Plany", subtitle }: Props) => {
-  const [selectedPlan, setSelectedPlan] = useState<string | null>("Free"); // Default to 'Free'
-
-  const handleSelectPlan = (plan: string) => {
-    setSelectedPlan(plan);
-  };
-
+export const Pricing = ({
+  planTitle,
+  values,
+  price,
+  selectedPlan,
+  handleSelectPlan,
+}: PricingPropsType) => {
   return (
-    <SectionTemplate title={title} description={subtitle}>
-      <section className={styles.pricingSection}>
-        <div className={styles.flex}>
-          <div
-            className={`${styles.pricingCard} ${selectedPlan === "Free" ? styles.selected : ""}`}
-            onClick={() => handleSelectPlan("Free")}
-          >
-            <div className={styles.planHeader}>
-              <h4 className={styles.title}>Free</h4>
-            </div>
-            <p className={styles.price}>
-              0 zł <span className={styles.priceDetails}>/miesiąc</span>
-            </p>
-            <button
-              className={`${styles.btn} ${selectedPlan === "Free" ? styles.btnSelected : ""}`}
-            >
-              {selectedPlan === "Free"
-                ? "Twój aktualny plan"
-                : "Wybierz ten plan"}
-            </button>
-
-            <ul className={styles.list}>
-              <h5 className={styles.planSubtitle}>Co zawiera ten plan?</h5>
-              <li className={styles.listItem}>
-                <CheckCircle className={styles.icon} />
-                <p className={styles.value}>Pełny dostęp do aplikacji</p>
-              </li>
-              <li className={styles.listItem}>
-                <CheckCircle className={styles.icon} />
-                <p className={styles.value}>Nieograniczona liczba mentee</p>
-              </li>
-              <li className={styles.listItem}>
-                <CheckCircle className={styles.icon} />
-                <p className={styles.value}>18% prowizji od spotkania</p>
-              </li>
-              <li className={styles.listItem}>
-                <CheckCircle className={styles.icon} />
-                <p className={styles.value}>Brak darmowych spotkań</p>
-              </li>
-            </ul>
+    <div
+      className={`${styles.pricingCard} ${selectedPlan === planTitle ? styles.selected : ""}`}
+      onClick={() => handleSelectPlan(planTitle)}
+    >
+      <div className={styles.planHeader}>
+        {planTitle !== "Free" ? (
+          <div className={styles.iconBox}>
+            {planTitle === "Mid" && <Basic />}
+            {planTitle === "Pro" && <Pro />}
           </div>
+        ) : null}
+        <h4 className={styles.title}>{planTitle}</h4>
+      </div>
+      <p className={styles.price}>
+        {`${price} zł `}
+        <span className={styles.priceDetails}>/miesiąc</span>
+      </p>
+      <button
+        className={`${styles.btn} ${selectedPlan === planTitle ? styles.btnSelected : ""}`}
+      >
+        {selectedPlan === planTitle ? "Twój aktualny plan" : "Wybierz ten plan"}
+      </button>
 
-          <div
-            className={`${styles.pricingCard} ${selectedPlan === "Mid" ? styles.selected : ""}`}
-            onClick={() => handleSelectPlan("Mid")}
-          >
-            <div className={styles.planHeader}>
-              <div className={styles.iconBox}>
-                <Basic />
-              </div>
-              <h4 className={styles.title}>Basic</h4>
-            </div>
-            <p className={styles.price}>
-              89 zł <span className={styles.priceDetails}>/miesiąc</span>
-            </p>
-            <button
-              className={`${styles.btn} ${selectedPlan === "Mid" ? styles.btnSelected : ""}`}
-            >
-              {selectedPlan === "Mid"
-                ? "Twój aktualny plan"
-                : "Wybierz ten plan"}
-            </button>
-
-            <ul className={styles.list}>
-              <h5 className={styles.planSubtitle}>Co zawiera ten plan?</h5>
-              <li className={styles.listItem}>
-                <CheckCircle className={styles.icon} />
-                <p className={styles.value}>5 darmowych spotkań w miesiącu</p>
-              </li>
-              <li className={styles.listItem}>
-                <CheckCircle className={styles.icon} />
-                <p className={styles.value}>
-                  Gwarancja stałej opłaty miesięcznej
-                </p>
-              </li>
-              <li className={styles.listItem}>
-                <CheckCircle className={styles.icon} />
-                <p className={styles.value}>Niższa prowizja: 10%</p>
-              </li>
-              <li className={styles.listItem}>
-                <CheckCircle className={styles.icon} />
-                <p className={styles.value}>Pełny dostęp do aplikacji</p>
-              </li>
-              <li className={styles.listItem}>
-                <CheckCircle className={styles.icon} />
-                <p className={styles.value}>Nieograniczona liczba mentee</p>
-              </li>
-            </ul>
-          </div>
-
-          <div
-            className={`${styles.pricingCard} ${selectedPlan === "Pro" ? styles.selected : ""}`}
-            onClick={() => handleSelectPlan("Pro")}
-          >
-            <div className={styles.planHeader}>
-              <div className={styles.iconBox}>
-                <Pro />
-              </div>
-              <h4 className={styles.title}>Pro</h4>
-            </div>
-            <p className={styles.price}>
-              190 zł <span className={styles.priceDetails}>/miesiąc</span>
-            </p>
-            <button
-              className={`${styles.btn} ${selectedPlan === "Pro" ? styles.btnSelected : ""}`}
-            >
-              {selectedPlan === "Pro"
-                ? "Twój aktualny plan"
-                : "Wybierz ten plan"}
-            </button>
-
-            <ul className={styles.list}>
-              <h5 className={styles.planSubtitle}>Co zawiera ten plan?</h5>
-              <li className={styles.listItem}>
-                <CheckCircle className={styles.icon} />
-                <p className={styles.value}>Darmowe spotkania bez limitu</p>
-              </li>
-              <li className={styles.listItem}>
-                <CheckCircle className={styles.icon} />
-                <p className={styles.value}>
-                  Gwarancja stałej opłaty miesięcznej
-                </p>
-              </li>
-              <li className={styles.listItem}>
-                <CheckCircle className={styles.icon} />
-                <p className={styles.value}>Brak prowizji</p>
-              </li>
-              <li className={styles.listItem}>
-                <CheckCircle className={styles.icon} />
-                <p className={styles.value}>Nieograniczona liczba mentee</p>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </section>
-    </SectionTemplate>
+      <ul className={styles.list}>
+        <h5 className={styles.planSubtitle}>Co zawiera ten plan?</h5>
+        {values.map((item) => (
+          <li className={styles.listItem}>
+            <span className={styles.icon}>
+              <CheckCircle />
+            </span>
+            <p className={styles.value}>{item}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
--- src/components/_connected/subscriptions/MentorSubscriptions.tsx
+++ src/components/_connected/subscriptions/MentorSubscriptions.tsx
@@ -1,64 +1,117 @@
-import React, {useEffect, useRef} from "react";
-import {Subscriptions} from "./Subscriptions";
-import {useSubscriptionsReducer} from "src/reducers/subscriptions";
-import {fetchMentorStudents} from "@services/mentor/fetchMentorStudents.service";
-import {SubscriptionStatus} from "@customTypes/subscriptions";
+import React, { useEffect, useRef, useState } from "react";
+import { Subscriptions } from "./Subscriptions";
+import { useSubscriptionsReducer } from "src/reducers/subscriptions";
+import { fetchMentorStudents } from "@services/mentor/fetchMentorStudents.service";
+import { SubscriptionStatus } from "@customTypes/subscriptions";
 import Pricing from "../pricing/Pricing";
+import { SectionTemplate } from "src/components/SectionTemplate";
+
+import styles from "./Subscriptions.module.scss";
 
 const PER_PAGE = 5;
 
 export const MentorSubscriptions = () => {
-    const sr = useSubscriptionsReducer();
-    const pageRef = useRef<number>(0);
-    const tabRef = useRef<SubscriptionStatus>('awaiting');
+  const [selectedPlan, setSelectedPlan] = useState<string>("Free"); // Default to 'Free'
 
-    const {tab, page} = sr.subscriptionsState;
+  const handleSelectPlan = (plan: string) => {
+    setSelectedPlan(plan);
+  };
+  const sr = useSubscriptionsReducer();
+  const pageRef = useRef<number>(0);
+  const tabRef = useRef<SubscriptionStatus>("awaiting");
 
-    useEffect(() => {
-        const fetchData = async () => {
-            sr.setPending(true);
-            const data = await fetchMentorStudents({
-                take: PER_PAGE,
-                skip: PER_PAGE * (page - 1),
-                status: tab,
-                sortBy: "status",
-                sortMethod: "ASC",
-            });
-            sr.updateRecords({
-                total: data.total,
-                records: data.students,
-                role: "M",
-            });
-            sr.setPending(false);
-        };
-        if (pageRef.current === 0 || pageRef.current !== page || tabRef.current !== tab) {
-            fetchData();
-            pageRef.current = page;
-            tabRef.current = tab;
+  const { tab, page } = sr.subscriptionsState;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      sr.setPending(true);
+      const data = await fetchMentorStudents({
+        take: PER_PAGE,
+        skip: PER_PAGE * (page - 1),
+        status: tab,
+        sortBy: "status",
+        sortMethod: "ASC",
+      });
+      sr.updateRecords({
+        total: data.total,
+        records: data.students,
+        role: "M",
+      });
+      sr.setPending(false);
+    };
+    if (
+      pageRef.current === 0 ||
+      pageRef.current !== page ||
+      tabRef.current !== tab
+    ) {
+      fetchData();
+      pageRef.current = page;
+      tabRef.current = tab;
+    }
+  }, [page, sr, tab]);
+
+  return (
+    <>
+      <Subscriptions
+        title="Status subskrypcji Twoich mentee"
+        subtitle={
+          <>
+            Tutaj masz wykaz aktualnie wykupionych przez mentee Subskrypcji, a
+            jeżeli chcesz zobaczyć historię swoich transakcji, przejdź do{" "}
+            <a href="/payment">Płatnosci.</a>
+          </>
         }
-    }, [page, sr, tab]);
-
-    return (
-        <>
-            <Subscriptions
-                title="Status subskrypcji Twoich mentee"
-                subtitle={
-                    <>
-                        Tutaj masz wykaz aktualnie wykupionych przez mentee Subskrypcji, a jeżeli chcesz zobaczyć
-                        historię swoich transakcji, przejdź do{" "}
-                        <a href="/payment">Płatnosci.</a>
-                    </>
-                }
-            />
-            <Pricing
-                title="Twoje Plany"
-                subtitle={
-                    <>
-                        Jeżeli chcesz zobaczyć historię swoich transakcji, przejdź do{" "}
-                        <a href="/payment"> Stripe.</a>
-                    </>
-                }
-            />
-        </>
-    );
+      />
+      <SectionTemplate
+        title="Twoje Plany"
+        description={
+          <>
+            Jeżeli chcesz zobaczyć historię swoich transakcji, przejdź do{" "}
+            <a href="/payment"> Stripe.</a>
+          </>
+        }
+        className={styles.sectionPricing}
+      >
+        <div className={styles.flex}>
+          <Pricing
+            planTitle="Free"
+            price={0}
+            values={[
+              "Pełny dostęp do aplikacji",
+              "Nieograniczona liczba mentee",
+              "18% prowizji od spotkania",
+              "Brak darmowych spotkań",
+            ]}
+            selectedPlan={selectedPlan}
+            handleSelectPlan={handleSelectPlan}
+          />
+          <Pricing
+            planTitle="Mid"
+            price={89}
+            values={[
+              "5 darmowych spotkań w miesiącu",
+              "Gwarancja stałej opłaty miesięcznej",
+              "Niższa prowizja: 10%",
+              "Pełny dostęp do aplikacji",
+              "Nieograniczona liczba mentee",
+            ]}
+            selectedPlan={selectedPlan}
+            handleSelectPlan={handleSelectPlan}
+          />
+          <Pricing
+            selectedPlan={selectedPlan}
+            price={190}
+            handleSelectPlan={handleSelectPlan}
+            planTitle="Pro"
+            values={[
+              "Darmowe spotkania bez limitu",
+              "Gwarancja stałej opłaty miesięcznej",
+              "Brak prowizji",
+              "Nieograniczona liczba mentee",
+            ]}
+          />
+        </div>
+      </SectionTemplate>
+    </>
+  );
 };

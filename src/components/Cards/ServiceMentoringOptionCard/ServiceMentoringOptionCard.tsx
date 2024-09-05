bf5b99e6f9585c5941d95e23684ad31dacaeb4dd
mkowack <kowackag@gmail.com>
2024-09-05 14:02:32 +0200

    feat(ServiceMentoringOptionCard): display number of session and time response

--- src/components/Cards/ServiceMentoringOptionCard/ServiceMentoringOptionCard.tsx
+++ src/components/Cards/ServiceMentoringOptionCard/ServiceMentoringOptionCard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {MentorshipPlan} from "@customTypes/order";
+import { MentorshipPlan } from "@customTypes/order";
 import styles from "./ServiceMentoringOptionCard.module.scss";
 import clx from "classnames";
 import { RadioInputIcon } from "@icons/RadioInput";
@@ -7,13 +7,16 @@ import { CrownIcon } from "@icons/CrownIcon";
 import { CheckCircleSolidIcon } from "@icons/CheckCircleSolidIcon";
 import Typography from "@mui/material/Typography";
 import StarSvg from "@icons/StarSvg";
-import {Collapse} from "@mui/material";
+import { Collapse } from "@mui/material";
 
 type Props = MentorshipPlan & {
   name?: string;
   value?: string;
   selected?: boolean;
   displayRadioInput?: boolean;
+  sessionsPerMonth: number;
+  sessionDurationMinutes: number;
+  responseTimeHours: number;
   handleSelect?: React.MouseEventHandler<HTMLButtonElement>;
   mentorProfileReview?: {
     profileImage: string | null;
@@ -36,9 +39,14 @@ export const ServiceMentoringOptionCard = ({
   selected,
   displayRadioInput,
   handleSelect,
-    mentorProfileReview,
+  sessionsPerMonth,
+  sessionDurationMinutes,
+  responseTimeHours,
+  mentorProfileReview,
 }: Props) => {
-
+  console.log(  sessionsPerMonth,
+    sessionDurationMinutes,
+    responseTimeHours,)
   return (
     <button
       className={clx(styles.card, {
@@ -50,30 +58,43 @@ export const ServiceMentoringOptionCard = ({
       value={value}
     >
       {mentorProfileReview && (
-          <Collapse in={!!mentorProfileReview}>
-            <div className={styles.mentorProfileReview}>
-                <img className={styles.avatarImage} src={mentorProfileReview.profileImage || "https://cdn.pixabay.com/photo/2023/04/21/15/42/portrait-7942151_640.jpg"} alt="mentor" />
-                <div className={styles.mentorDescription}>
-                  <div className={styles.nameRow}>
-                      <Typography variant='buttonMd' align='left' >
-                        {[mentorProfileReview.firstName, mentorProfileReview.lastName].filter(Boolean).join(' ')}
-                      </Typography>
-                      {!!mentorProfileReview.reviewsAvgRate && (
-                        <>
-                          <StarSvg />
-                          <Typography variant='buttonMd' sx={{ ml: 1 }}>{mentorProfileReview.reviewsAvgRate}</Typography>
-                        </>
-                      )}
-                  </div>
-                  <Typography variant='caption'>
-                    {mentorProfileReview.jobPosition}
-                  </Typography>
-                </div>
+        <Collapse in={!!mentorProfileReview}>
+          <div className={styles.mentorProfileReview}>
+            <img
+              className={styles.avatarImage}
+              src={
+                mentorProfileReview.profileImage ||
+                "https://cdn.pixabay.com/photo/2023/04/21/15/42/portrait-7942151_640.jpg"
+              }
+              alt="mentor"
+            />
+            <div className={styles.mentorDescription}>
+              <div className={styles.nameRow}>
+                <Typography variant="buttonMd" align="left">
+                  {[mentorProfileReview.firstName, mentorProfileReview.lastName]
+                    .filter(Boolean)
+                    .join(" ")}
+                </Typography>
+                {!!mentorProfileReview.reviewsAvgRate && (
+                  <>
+                    <StarSvg />
+                    <Typography variant="buttonMd" sx={{ ml: 1 }}>
+                      {mentorProfileReview.reviewsAvgRate}
+                    </Typography>
+                  </>
+                )}
+              </div>
+              <Typography variant="caption">
+                {mentorProfileReview.jobPosition}
+              </Typography>
             </div>
-          </Collapse>
+          </div>
+        </Collapse>
       )}
       <div className={styles.rowTitle}>
-        {handleSelect && displayRadioInput ? <RadioInputIcon filled={selected} /> : null}
+        {handleSelect && displayRadioInput ? (
+          <RadioInputIcon filled={selected} />
+        ) : null}
         <h5 className={styles.title}>{title}</h5>
         {variant === "pro" ? <CrownIcon className={styles.crown} /> : null}
       </div>
@@ -84,19 +105,35 @@ export const ServiceMentoringOptionCard = ({
         <span>miesięcznie</span>
       </div>
 
-      {descriptionRows && descriptionRows.length > 0 ? (
-        <div className={styles.listRow}>
-          <h6>Plan obejmuje</h6>
-          <ul>
-            {descriptionRows.map((r,ind) => (
-              <li key={ind}>
-                <CheckCircleSolidIcon />
-                <span>{r.description}</span>
-              </li>
-            ))}
-          </ul>
-        </div>
-      ) : null}
+      <div className={styles.listRow}>
+        <h6>Plan obejmuje</h6>
+        <ul>
+          <li>
+            <CheckCircleSolidIcon />
+            <span>
+              {`${sessionsPerMonth} sesje mentoringowe na miesiąc ${
+                typeof sessionDurationMinutes !== "undefined"
+                  ? `(${sessionDurationMinutes} minut każda)`
+                  : null
+              }`}
+            </span>
+          </li>
+          <li>
+            <CheckCircleSolidIcon />
+            <span>
+              {`Odpowiedzi na Twoje pytania w ciągu: ${responseTimeHours}h `}
+            </span>
+          </li>
+          {descriptionRows && descriptionRows.length > 0
+            ? descriptionRows.map((r, ind) => (
+                <li key={ind}>
+                  <CheckCircleSolidIcon />
+                  <span>{r.description}</span>
+                </li>
+              ))
+            : null}
+        </ul>
+      </div>
     </button>
   );
 };

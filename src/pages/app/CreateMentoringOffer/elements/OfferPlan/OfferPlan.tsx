e73ea4f73303c99c4ddec14faf8e2f193b40c47c
mkowack <kowackag@gmail.com>
2024-09-06 11:39:35 +0200

    fix(OfferPlan): change into interactive element - button

--- src/pages/app/CreateMentoringOffer/elements/OfferPlan/OfferPlan.tsx
+++ src/pages/app/CreateMentoringOffer/elements/OfferPlan/OfferPlan.tsx
@@ -52,7 +52,7 @@ export const OfferPlan = ({
   }
 
   return (
-    <div onClick={() => setSelected(plan)}>
+    <button className={styles.offerPlan} onClick={() => setSelected(plan)}>
       <div className={styles.containerSchedule}>
         <p className={styles.scheduleSubtitle}>
           Harmonogram dla Planu{" "}
@@ -132,6 +132,6 @@ export const OfferPlan = ({
           )}
         </ul>
       ) : null}
-    </div>
+    </button>
   );
 };

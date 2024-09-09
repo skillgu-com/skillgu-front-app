d6b00d6a81f0b86c21b55d729dfac4ddc3cfa33f
mkowack <kowackag@gmail.com>
2024-09-09 18:17:58 +0200

    fix: show only truthy values of planIncludes-mentorshipOffer

--- src/utils/text.tsx
+++ src/utils/text.tsx
@@ -1,4 +1,4 @@
 export function replaceSpacesWithNonBreaking(text: string): string {
     // Replace spaces after single characters with non-breaking spaces
-    return text.replace(/(\b\w\b) /g, '$1\u00A0');
+    return text  && text.replace(/(\b\w\b) /g, '$1\u00A0');
 }

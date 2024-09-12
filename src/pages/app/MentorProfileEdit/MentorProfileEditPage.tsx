37dc6ca6f89cad80f3640746f551c236869e73f7
mkowack <kowackag@gmail.com>
2024-09-12 11:36:53 +0200

    feat: add img to ProfileEdit

--- src/pages/app/MentorProfileEdit/MentorProfileEditPage.tsx
+++ src/pages/app/MentorProfileEdit/MentorProfileEditPage.tsx
@@ -143,7 +143,7 @@ export const MentorProfileEditPage = () => {
             initialDataFetched.current = true;
         }
     }, [mentorId, username]);
-
+console.log(1, mentorData)
 
     return loading ? (
       <ProfileEditPageSkeleton />
@@ -152,13 +152,15 @@ export const MentorProfileEditPage = () => {
             {/*//TODO dodane chwilowo defaultowe avatarUrl oraz coverUrl jako linki dlatego nie widzimy zmian w ustawnieach*/}
             <UserProfileHeader
                 avatarUrl={
-                    "https://res.cloudinary.com/dkclg8ppw/image/upload/v1725528576/default/avatar.jpg" ||
-                    "https://res.cloudinary.com/dkclg8ppw/image/upload/v1725528576/default/avatar.jpg"
+                  mentorData?.profileImage ||
+                  "https://res.cloudinary.com/dkclg8ppw/image/upload/v1725528576/default/avatar.jpg"
                 }
                 btnText="Zobacz profil"
                 btnHref={`/mentor/${username}`}
                 company={mentorData?.company}
-                coverUrl={"https://res.cloudinary.com/dkclg8ppw/image/upload/v1725528618/default/cover.png" || "https://res.cloudinary.com/dkclg8ppw/image/upload/v1725528618/default/cover.png"}
+                coverUrl={
+                    mentorData?.coverImage ||
+                    "https://res.cloudinary.com/dkclg8ppw/image/upload/v1725528618/default/cover.png"}
                 fullname={mentorData?.firstName + " " + mentorData?.lastName}
                 langSwitcher={
                     <MentorLangs

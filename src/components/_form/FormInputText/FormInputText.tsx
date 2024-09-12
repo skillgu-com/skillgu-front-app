ec174854da6b009364a488e239063f816aa9cb1c
patrykrudzinski <patrykrudzinski@gmail.com>
2024-09-12 15:06:35 +0200

    password input + scroll in sidebar

--- src/components/_form/FormInputText/FormInputText.tsx
+++ src/components/_form/FormInputText/FormInputText.tsx
@@ -4,13 +4,16 @@ import {
     FieldValues, Control, ControllerProps, FormState
 } from "react-hook-form"
 import TextField, {TextFieldProps} from '@mui/material/TextField';
-import React, {useMemo} from "react";
-import {Collapse} from "@mui/material";
+import React, {useMemo, useState} from "react";
+import {Collapse, IconButton} from "@mui/material";
 import Typography from "@mui/material/Typography";
 import styles from '../styles.module.scss'
 import InputFeedback, {InputFeedbackProps} from "../InputFeedback/InputFeedback";
 import {StyledInputWrapper} from "../FormInputCheckbox/FormInputCheckbox.styles";
 import {StyledFeedbackWrapper} from "../FormInputSwitcher/FormInputSwitcher.styles";
+
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 interface Props<T extends FieldValues> {
     name: Path<T>;
@@ -39,13 +42,37 @@ const FormInputText = <T extends FieldValues>({
         return [{message: error.message as string, severity: 'error'}];
     }, [formState, customFeedback, name]);
 
+
+    const [showPassword, setShowPassword] = useState(false);
+    const toggleShowPassword = () => setShowPassword((show) => !show);
+
+    const endAdornment = useMemo(() => {
+        if (inputProps?.InputProps?.endAdornment) return inputProps.InputProps.endAdornment;
+        if (inputProps?.type === 'password') {
+            return (
+                <IconButton onClick={toggleShowPassword}>
+                    {showPassword ? <VisibilityOff/> : <Visibility/>}
+                </IconButton>
+            );
+        }
+        return undefined;
+    }, [inputProps, showPassword])
+
     return (
         <StyledInputWrapper>
             {label && <Typography variant='buttonMd' color='secondary' className={styles.Label}>{label}</Typography>}
             <Controller
                 name={name}
                 control={control}
-                render={({field: {ref, ...field}}) => <TextField {...field} {...inputProps} inputRef={ref}/>}
+                render={({field: {ref, ...field}}) => <TextField
+                    {...field}
+                    {...inputProps}
+                    type={showPassword ? 'text' : inputProps?.type}
+                    inputRef={ref}
+                    InputProps={{
+                        endAdornment
+                    }}
+                />}
                 {...controllerProps}
             />
             <Collapse in={feedback && !!feedback.length}>

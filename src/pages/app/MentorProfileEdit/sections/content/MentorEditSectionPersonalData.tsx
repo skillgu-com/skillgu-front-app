38f31d09a2bbf01a8d770f9bcbe3731168ba79d5
mkowack <kowackag@gmail.com>
2024-09-22 16:01:10 +0200

    refactor(MentorEditSectionPersonalData): remove unused code

--- src/pages/app/MentorProfileEdit/sections/content/MentorEditSectionPersonalData.tsx
+++ src/pages/app/MentorProfileEdit/sections/content/MentorEditSectionPersonalData.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import {useForm} from "react-hook-form";
-import styles from "./styles.module.scss";
-import updateUserPersonalData from "../../../../../services/mentor/settingMentor.service";
-// import {useParams} from "react-router-dom";
+
 import {MentorData} from "../../MentorProfileEditPage";
 import {UserEditSection} from "../../../../../components/_grouped";
 import FormInputText from "../../../../../components/_form/FormInputText/FormInputText";
 import FormInputFile from "../../../../../components/_form/FormUploadFile/FormUploadFile";
 
+import styles from "./styles.module.scss";
+
+import updateUserPersonalData from "../../../../../services/mentor/settingMentor.service";
 
 export type MentorEditPersonalDataFormInput = {
     firstName: string;
@@ -24,13 +25,6 @@ export type MentorEditPersonalDataFormInputTest = {
     coverUrl:string;
 };
 
-// interface FormData {
-//     firstName: HTMLInputElement;
-//     surname: HTMLInputElement;
-//     avatarUrl: HTMLInputElement;
-//     coverUrl: HTMLInputElement;
-// }
-
 type MentorPersonalData = {
     firstName: string;
     surname: string;
@@ -42,9 +36,8 @@ type Props = {
     mentorData: MentorData;
 };
 
-
 export const MentorEditSectionPersonalData = ({ mentorData }: Props) => {
-    const {control, formState, handleSubmit, watch} =
+    const {control, formState, handleSubmit} =
         useForm<MentorEditPersonalDataFormInput>({
             defaultValues: {
                 firstName: "",
@@ -57,13 +50,8 @@ export const MentorEditSectionPersonalData = ({ mentorData }: Props) => {
     const inputProps = {
         formState: formState,
         control: control,
-        // controllerProps: {rules: {required: "Imię jest wymagane"}},
         controllerProps: {}
     };
-
-    // const avatarValue = watch('avatarUrl')
-    // const coverValue = watch('coverUrl')
-    // const {id: userID} = useParams();
 
     const onSubmit = async (data: MentorEditPersonalDataFormInput) => {
         try {
@@ -73,7 +61,7 @@ export const MentorEditSectionPersonalData = ({ mentorData }: Props) => {
                 avatarUrl: data.avatarUrl,
                 coverUrl: data.coverUrl,
             };
-            const response = await updateUserPersonalData(mentorPersonalData);
+            await updateUserPersonalData(mentorPersonalData);
             window.location.reload();
         } catch (error) {
             console.error('Failed to update personal data', error);
@@ -83,7 +71,6 @@ export const MentorEditSectionPersonalData = ({ mentorData }: Props) => {
 
     return (
         <UserEditSection
-            key={Math.random()}
             title="Dane osobowe"
             subtitle="Zaktualizuj swoje zdjęcie oraz dane osobowe."
             onClose={() => {

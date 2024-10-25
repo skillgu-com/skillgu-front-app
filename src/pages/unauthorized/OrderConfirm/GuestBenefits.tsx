7fe3b3ecc74c454646eac1a1e8d96990998b88d7
Tomasz Rozpara <kowackag@gmail.com>
2024-10-25 09:20:48 +0200

    fix(OrderConfirm):fix checkbox icon width

--- src/pages/unauthorized/OrderConfirm/GuestBenefits.tsx
+++ src/pages/unauthorized/OrderConfirm/GuestBenefits.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import {ListStyleArtistIcon} from "@icons/ListStyleArtistIcon";
+import clx from 'classnames'
+import { ListStyleArtistIcon } from "@icons/ListStyleArtistIcon";
 import Button from "src/components/Button/Button";
 
 import styles from "./OrderConfirm.module.scss";
@@ -7,30 +8,38 @@ import styles from "./OrderConfirm.module.scss";
 import paths from "src/paths";
 
 const GuestBenefits: React.FC = () => (
-    <div className={styles.box}>
-        <p className={styles.title}>Zakładając konto zyskujesz wiele korzyści!</p>
-        <ul className={styles.list}>
-            <li className={styles.listItem}>
-                <ListStyleArtistIcon />
-                <p>Dostęp do kalendarza wszystkich spotkań</p>
-            </li>
-            <li className={styles.listItem}>
-                <ListStyleArtistIcon />
-                <p>System przypomnień i powiadomień</p>
-            </li>
-            <li className={styles.listItem}>
-                <ListStyleArtistIcon />
-                <p>Bezpośredni kontakt z mentorami</p>
-            </li>
-            <li className={styles.listItem}>
-                <ListStyleArtistIcon />
-                <p>Możliwość zapisania się na regularne sesje mentoringowe</p>
-            </li>
-        </ul>
-        <Button href={paths.registerMentee} classes={styles.btn}>
-            Stwórz konto
-        </Button>
-    </div>
+  <div className={clx(styles.box, styles.quest)}>
+    <p className={styles.title}>Zakładając konto zyskujesz wiele korzyści!</p>
+    <ul className={styles.list}>
+      <li className={styles.listItem}>
+        <div className={styles.listIconBox}>
+          <ListStyleArtistIcon />
+        </div>
+        <p>Dostęp do kalendarza wszystkich spotkań</p>
+      </li>
+      <li className={styles.listItem}>
+        <div className={styles.listIconBox}>
+          <ListStyleArtistIcon />
+        </div>
+        <p>System przypomnień i powiadomień</p>
+      </li>
+      <li className={styles.listItem}>
+        <div className={styles.listIconBox}>
+          <ListStyleArtistIcon />
+        </div>
+        <p>Bezpośredni kontakt z mentorami</p>
+      </li>
+      <li className={styles.listItem}>
+        <div className={styles.listIconBox}>
+          <ListStyleArtistIcon />
+        </div>
+        <p>Możliwość zapisania się na regularne sesje mentoringowe</p>
+      </li>
+    </ul>
+    <Button href={paths.registerMentee} classes={styles.btn}>
+      Stwórz konto
+    </Button>
+  </div>
 );
 
 export default GuestBenefits;

5981b430cd5af3705a2566ca1403eee88f47ce06
Tomasz Rozpara <kowackag@gmail.com>
2024-10-26 17:20:43 +0200

    feat: sending msg - handle response

--- src/services/contact/contact.service.ts
+++ src/services/contact/contact.service.ts
@@ -1,28 +1,34 @@
 import axios from "axios";
-import {ContactServiceInput, ContactServiceOutput} from "./contact.types";
+import { ContactServiceInput, ContactServiceOutput } from "./contact.types";
 
-export const contactService = async ({email, message,}: ContactServiceInput): Promise<ContactServiceOutput> => {
-    try {
-        const response = await axios.post("/api/contact/app/send", {email, message,});
-        console.log(email)
-        console.log(message)
+export const contactService = async ({
+  email,
+  message,
+}: ContactServiceInput): Promise<ContactServiceOutput> => {
+  try {
+    const response = await axios.post("/api/contact/app/send", {
+      email,
+      message,
+    });
 
-        if (response.status === 200 || response.status === 201) {
-            return {
-                success: true,
-                successMessage: "Dziękujemy za wiadomość. Numer Twojego zgłoszenia to #1029",
-            };
-        } else {
-            return {
-                success: false,
-                errorMessage: "Nie udało się wysłać wiadomości. Spróbuj ponownie później.",
-            };
-        }
-    } catch (error) {
-        console.error("Błąd przy wysyłaniu wiadomości: ", error);
-        return {
-            success: false,
-            errorMessage: "Nie udało się wysłać wiadomości z powodu błędu serwera. Skontaktuj się z nami na email help@skillgu.com",
-        };
+    if (response.status === 200 || response.status === 201) {
+      return {
+        success: true,
+        successMessage: "Dziękujemy! Wiadomość została wysłana",
+      };
+    } else {
+      return {
+        success: false,
+        errorMessage:
+          "Nie udało się wysłać wiadomości. Spróbuj ponownie później.",
+      };
     }
+  } catch (error) {
+    console.error("Błąd przy wysyłaniu wiadomości: ", error);
+    return {
+      success: false,
+      errorMessage:
+        "Nie udało się wysłać wiadomości z powodu błędu serwera. Skontaktuj się z nami na email help@skillgu.com",
+    };
+  }
 };

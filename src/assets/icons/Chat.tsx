102fd0e57c9a8f74a4f7bb0ff350a1fd67756054
Tomasz Rozpara <kowackag@gmail.com>
2024-11-03 14:24:58 +0100

    feat:ChatIcon

---
+++ src/assets/icons/Chat.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+
+export const ChatIcon = ({className}:{className?:string}) => {
+  return (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 20 21"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+    >
+      <path
+        d="M14.9827 9.49167V12.825C14.9827 13.0417 14.9744 13.25 14.9494 13.45C14.7577 15.7 13.4327 16.8167 10.991 16.8167H10.6577C10.4494 16.8167 10.2493 16.9167 10.1243 17.0833L9.12436 18.4167C8.68269 19.0083 7.96601 19.0083 7.52435 18.4167L6.52434 17.0833C6.416 16.9417 6.17435 16.8167 5.99102 16.8167H5.65769C2.99936 16.8167 1.66602 16.1583 1.66602 12.825V9.49167C1.66602 7.05001 2.79102 5.72501 5.03269 5.53334C5.23269 5.50834 5.44102 5.5 5.65769 5.5H10.991C13.6494 5.5 14.9827 6.83334 14.9827 9.49167Z"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M18.3165 6.15867V9.492C18.3165 11.942 17.1915 13.2587 14.9499 13.4503C14.9749 13.2503 14.9832 13.042 14.9832 12.8253V9.492C14.9832 6.83367 13.6499 5.50033 10.9915 5.50033H5.6582C5.44154 5.50033 5.2332 5.50867 5.0332 5.53367C5.22487 3.292 6.54987 2.16699 8.99154 2.16699H14.3249C16.9832 2.16699 18.3165 3.50033 18.3165 6.15867Z"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M11.2469 11.5417H11.2544"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M8.32893 11.5417H8.33643"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M5.41292 11.5417H5.42042"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};

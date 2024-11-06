c37a8e356cb0a441f9c1d67690ebc321f7a389f1
Małgorzata Kowacka <kowackag@gmail.com>
2024-11-07 00:00:59 +0100

    feat:change icons help and logout

--- src/assets/icons/Help.tsx
+++ src/assets/icons/Help.tsx
@@ -2,40 +2,37 @@ import React from 'react';
 
 const Help = () => {
 	return (
-		<svg
-			xmlns='http://www.w3.org/2000/svg'
-			width='22'
-			height='22'
-			viewBox='0 0 22 22'
-			fill='none'>
-			<circle
-				cx='11'
-				cy='11'
-				r='10'
-				stroke='white'
-				className='on-hover--stroke'
-				strokeWidth='1.5'
-				strokeLinecap='round'
-				strokeLinejoin='round'
-			/>
-			<path
-				d='M8.08984 7.99999C8.57528 6.62004 9.98523 5.7914 11.427 6.0387C12.8688 6.28601 13.922 7.53714 13.9198 8.99999C13.9198 11 10.9198 12 10.9198 12'
-				stroke='white'
-				className='on-hover--stroke'
-				strokeWidth='1.5'
-				strokeLinecap='round'
-				strokeLinejoin='round'
-			/>
-			<path
-				d='M10.9999 16H11.0099'
-				stroke='white'
-				className='on-hover--stroke'
-				strokeWidth='1.5'
-				strokeLinecap='round'
-				strokeLinejoin='round'
-			/>
-		</svg>
-	);
+    <svg
+      width="20"
+      height="21"
+      viewBox="0 0 20 21"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M14.1665 15.8588H10.8332L7.12483 18.3254C6.57483 18.6921 5.83317 18.3004 5.83317 17.6338V15.8588C3.33317 15.8588 1.6665 14.1921 1.6665 11.6921V6.69206C1.6665 4.19206 3.33317 2.52539 5.83317 2.52539H14.1665C16.6665 2.52539 18.3332 4.19206 18.3332 6.69206V11.6921C18.3332 14.1921 16.6665 15.8588 14.1665 15.8588Z"
+        stroke="white"
+        strokeWidth="1.5"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M10 9.9668V9.79183C10 9.22516 10.35 8.92515 10.7 8.68348C11.0417 8.45015 11.3833 8.15016 11.3833 7.60016C11.3833 6.83349 10.7667 6.2168 10 6.2168C9.23334 6.2168 8.6167 6.83349 8.6167 7.60016"
+        stroke="white"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M9.99641 11.9577H10.0039"
+        stroke="white"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
 };
 
 export default Help;
--- src/assets/icons/Logout.tsx
+++ src/assets/icons/Logout.tsx
@@ -2,38 +2,36 @@ import React from 'react';
 
 const Logout = () => {
 	return (
-		<svg
-			xmlns='http://www.w3.org/2000/svg'
-			width='20'
-			height='20'
-			viewBox='0 0 20 20'
-			fill='none'>
-			<path
-				d='M7 19H3C1.89543 19 1 18.1046 1 17V3C1 1.89543 1.89543 1 3 1H7'
-				stroke='white'
-				className='on-hover--stroke'
-				strokeWidth='1.5'
-				strokeLinecap='round'
-				strokeLinejoin='round'
-			/>
-			<path
-				d='M14 15L19 10L14 5'
-				stroke='white'
-				className='on-hover--stroke'
-				strokeWidth='1.5'
-				strokeLinecap='round'
-				strokeLinejoin='round'
-			/>
-			<path
-				d='M19 10H7'
-				stroke='white'
-				className='on-hover--stroke'
-				strokeWidth='1.5'
-				strokeLinecap='round'
-				strokeLinejoin='round'
-			/>
-		</svg>
-	);
+    <svg
+      width="20"
+      height="21"
+      viewBox="0 0 20 21"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M7.4165 6.79922C7.67484 3.79922 9.2165 2.57422 12.5915 2.57422H12.6998C16.4248 2.57422 17.9165 4.06589 17.9165 7.79089V13.2242C17.9165 16.9492 16.4248 18.4409 12.6998 18.4409H12.5915C9.2415 18.4409 7.69984 17.2326 7.42484 14.2826"
+        stroke="white"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M12.4999 10.5H3.0166"
+        stroke="white"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M4.87516 7.70898L2.0835 10.5007L4.87516 13.2923"
+        stroke="white"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
 };
 
 export default Logout;

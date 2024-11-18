d0e27111cdf0714c4ffd85c6e1160ae56dbbcbd3
Małgorzata Kowacka <kowackag@gmail.com>
2024-11-18 18:18:22 +0100

    feat: subscription style

--- src/components/_connected/subscriptions/MentorSubscriptions.tsx
+++ src/components/_connected/subscriptions/MentorSubscriptions.tsx
@@ -137,191 +137,206 @@ export const MentorSubscriptions = () => {
   };
 
   return (
-      <>
-        {suspendingPlan && (
-            <Modal
-                title={
-                  <p className={styles.modalSubtitle}>
-                    Czy na pewno chcesz zmienić swój aktualny plan na{" "}
-                    <span className={styles.modalPlan}>{suspendingPlan}</span>?
-                  </p>
-                }
-                className={styles.modal}
-                closeHandler={() => setSuspendingPlan(null)}
+    <>
+      {suspendingPlan && (
+        <Modal
+          title={
+            <p className={styles.modalSubtitle}>
+              Czy na pewno chcesz zmienić swój aktualny plan na{" "}
+              <span className={styles.modalPlan}>{suspendingPlan}</span>?
+            </p>
+          }
+          className={styles.modal}
+          closeHandler={() => setSuspendingPlan(null)}
+        >
+          <div className={styles.modalBtnBox}>
+            <Button
+              as={ButtonTag.Button}
+              variant={ButtonVariant.Outline}
+              onClick={() => setSuspendingPlan(null)}
             >
-              <div className={styles.modalBtnBox}>
-                <Button
-                    as={ButtonTag.Button}
-                    variant={ButtonVariant.Outline}
-                    onClick={() => setSuspendingPlan(null)}
-                >
-                  Anuluj
-                </Button>
-                <Button
-                    as={ButtonTag.Button}
-                    onClick={() => changeMentorPlan(suspendingPlan)}
-                >
-                  Zmień plan
-                </Button>
-              </div>
-            </Modal>
-        )}
-        {isCancelingPlan && (
-            <Modal
-                withClose={false}
-                title={
-                  <p className={styles.title}>
-                    Czy na pewno chcesz anulować swój plan?
-                  </p>
-                }
-                classNameContent={styles.modalCancel}
-                closeHandler={() => setIsCancelingPlan(false)}
+              Anuluj
+            </Button>
+            <Button
+              as={ButtonTag.Button}
+              onClick={() => changeMentorPlan(suspendingPlan)}
             >
-              <p className={styles.info}>
-                {`Możesz nadal korzystać z korzyści wynikających z planu do dnia
+              Zmień plan
+            </Button>
+          </div>
+        </Modal>
+      )}
+      {isCancelingPlan && (
+        <Modal
+          withClose={false}
+          title={
+            <p className={styles.title}>
+              Czy na pewno chcesz anulować swój plan?
+            </p>
+          }
+          classNameContent={styles.modalCancel}
+          closeHandler={() => setIsCancelingPlan(false)}
+        >
+          <p className={styles.info}>
+            {`Możesz nadal korzystać z korzyści wynikających z planu do dnia
             ${subscriptionDetails?.endDate}.`}
-              </p>
-              <div className={styles.modalCancelBtnBox}>
-                <Button
-                    as={ButtonTag.Button}
-                    variant={ButtonVariant.Transparent}
-                    onClick={() => setIsCancelingPlan(false)}
-                >
-                  Zamknij
-                </Button>
-                <Button
-                    as={ButtonTag.Button}
-                    variant={ButtonVariant.Transparent}
-                    classes={styles.mainBtn}
-                    onClick={handleCancelSubscription}
-                >
-                  Tak, anuluj plan
-                </Button>
-              </div>
-            </Modal>
-        )}
-        <Subscriptions
-            title="Status subskrypcji Twoich mentee"
-            subtitle={
+          </p>
+          <div className={styles.modalCancelBtnBox}>
+            <Button
+              as={ButtonTag.Button}
+              variant={ButtonVariant.Transparent}
+              onClick={() => setIsCancelingPlan(false)}
+            >
+              Zamknij
+            </Button>
+            <Button
+              as={ButtonTag.Button}
+              variant={ButtonVariant.Transparent}
+              classes={styles.mainBtn}
+              onClick={handleCancelSubscription}
+            >
+              Tak, anuluj plan
+            </Button>
+          </div>
+        </Modal>
+      )}
+      <Subscriptions
+        title="Status subskrypcji Twoich mentee"
+        subtitle={
+          <>
+            Tutaj masz wykaz aktualnie wykupionych przez mentee subskrypcji, a
+            jeżeli chcesz zobaczyć historię swoich transakcji, przejdź do{" "}
+            <a href="/payment">Płatności.</a>
+          </>
+        }
+      />
+      <div className={styles.sectionContainer}>
+        <section className={styles.currentPlan}>
+          <div className={styles.titleBox}>
+            <h2 className={styles.title}>Aktualny plan</h2>
+            <p className={styles.subtitle}>
+              Tutaj możesz zobaczyć swój aktualny plan, anulować subskrypcję lub
+              przejść na wyższy plan.
+            </p>
+          </div>
+          <div className={styles.currentPlanInfoBox}>
+            {subscriptionDetails?.status === "canceled" ? (
               <>
-                Tutaj masz wykaz aktualnie wykupionych przez mentee subskrypcji, a
-                jeżeli chcesz zobaczyć historię swoich transakcji, przejdź do{" "}
-                <a href="/payment">Płatności.</a>
+                <p className={styles.planItem}>
+                  Twoja subskrypcja została anulowana.
+                </p>
+                <p className={styles.planItem}>
+                  Ostatnia subskrypcja wygaśnie: {subscriptionDetails?.endDate}
+                </p>
+                <p className={styles.planItem}>
+                  Ostatnia nazwa subskrypcji :{" "}
+                  {subscriptionDetails?.lastPlanName}
+                </p>
               </>
-            }
-        />
-        <div className={styles.sectionContainer}>
-          <section className={styles.currentPlan}>
-            <div className={styles.titleBox}>
-              <h2 className={styles.title}>Aktualny plan</h2>
-              <p className={styles.subtitle}>
-                Tutaj możesz zobaczyć swój aktualny plan, anulować subskrypcję lub
-                przejść na wyższy plan.
-              </p>
-            </div>
-            <div className={styles.currentPlanInfoBox}>
-              {subscriptionDetails?.status === "canceled" ? (
-                  <>
-                    <p>Twoja subskrypcja została anulowana.</p>
-                    <p>Ostatnia subskrypcja wygaśnie: {subscriptionDetails?.endDate}</p>
-                    <p>Ostatnia nazwa subskrypcji : {subscriptionDetails?.lastPlanName}</p>
-                  </>
-              ) : subscriptionDetails?.planName === "Free" ? (
-                  <>
-                    <p>Obecnie korzystasz z darmowego planu.</p>
-                    <p>Pełny dostęp do aplikacji bez kosztów.</p>
-                  </>
-              ) : (
-                  <>
-                    <div className={styles.planHeader}>
-                      <div className={styles.iconBox}>
-                        {subscriptionDetails?.planName === "Mid" && <Basic />}
-                        {subscriptionDetails?.planName === "Pro" && <Pro />}
-                      </div>
-                      <h4 className={styles.planName}>
-                        Plan {subscriptionDetails?.planName || "N/A"}
-                      </h4>
-                    </div>
-                    <ul className={styles.planInfoList}>
-                      <li className={styles.planItem}>
-                        Data odnowy subskrypcji:{" "}
-                        <span className={styles.planItemValue}>
+            ) : subscriptionDetails?.planName === "Free" ? (
+              <>
+                <p className={styles.planItem}>
+                  Obecnie korzystasz z darmowego planu.
+                </p>
+                <p className={styles.planItem}>
+                  Pełny dostęp do aplikacji bez kosztów.
+                </p>
+              </>
+            ) : (
+              <>
+                <div className={styles.planHeader}>
+                  <div className={styles.iconBox}>
+                    {subscriptionDetails?.planName === "Mid" && <Basic />}
+                    {subscriptionDetails?.planName === "Pro" && <Pro />}
+                  </div>
+                  <h4 className={styles.planName}>
+                    Plan {subscriptionDetails?.planName || "N/A"}
+                  </h4>
+                </div>
+                <ul className={styles.planInfoList}>
+                  <li className={styles.planItem}>
+                    Data odnowy subskrypcji:{" "}
+                    <span className={styles.planItemValue}>
                       {subscriptionDetails?.endDate || "N/A"}
                     </span>
-                      </li>
-                      <li className={styles.planItem}>
-                        Kwota do zapłaty{" "}
-                        <span className={styles.planItemValue}>
+                  </li>
+                  <li className={styles.planItem}>
+                    Kwota do zapłaty{" "}
+                    <span className={styles.planItemValue}>
                       {subscriptionDetails?.planAmount || "N/A"}
                     </span>
-                      </li>
-                    </ul>
-                    {subscriptionDetails?.status !== "canceled" && (
-                        <Button
-                            onClick={() => setIsCancelingPlan(true)}
-                            variant={ButtonVariant.PrimaryLight}
-                            as={ButtonTag.Button}
-                            type="button"
-                            fontVariant="button-md"
-                        >
-                          Anuluj subskrypcję
-                        </Button>
-                    )}
-                  </>
-              )}
-            </div>
-          </section>
-          <SectionTemplate title="" description="" className={styles.sectionPricing}>
-            <div className={styles.flex}>
-              <Pricing
-                  planTitle="Free"
-                  price={0}
-                  values={[
-                    "Pełny dostęp do aplikacji",
-                    "Nieograniczona liczba mentee",
-                    "10% prowizji od spotkania",
-                    "Brak darmowych spotkań",
-                  ]}
-                  selectedPlan={selectedPlan}
-                  handleSelectPlan={handleSelectPlan}
-                  canChangePlan={canSelectPlan("Free")}
-                  currentPlan={subscriptionDetails?.planName || "Free"}
-              />
-              <Pricing
-                  planTitle="Mid"
-                  price={89}
-                  values={[
-                    "20 darmowych spotkań w miesiącu",
-                    "Gwarancja stałej opłaty miesięcznej",
-                    "Niższa prowizja: 5%",
-                    "Pełny dostęp do aplikacji",
-                    "Nieograniczona liczba mentee",
-                  ]}
-                  selectedPlan={selectedPlan}
-                  handleSelectPlan={handleSelectPlan}
-                  canChangePlan={canSelectPlan("Mid")}
-                  currentPlan={subscriptionDetails?.planName || "Free"}
-              />
-              <Pricing
-                  planTitle="Pro"
-                  price={189}
-                  values={[
-                    "Darmowe spotkania bez limitu",
-                    "Gwarancja stałej opłaty miesięcznej",
-                    "Brak prowizji",
-                    "Nieograniczona liczba mentee",
-                  ]}
-                  selectedPlan={selectedPlan}
-                  handleSelectPlan={handleSelectPlan}
-                  canChangePlan={canSelectPlan("Pro")}
-                  currentPlan={subscriptionDetails?.planName || "Free"}
-              />
-            </div>
-          </SectionTemplate>
-          <div className={styles.subscriptionHistoryContainer}>
-            <MentorPaymentSubscription />
+                  </li>
+                </ul>
+                {subscriptionDetails?.status !== "canceled" && (
+                  <Button
+                    onClick={() => setIsCancelingPlan(true)}
+                    variant={ButtonVariant.PrimaryLight}
+                    as={ButtonTag.Button}
+                    type="button"
+                    fontVariant="button-md"
+                  >
+                    Anuluj subskrypcję
+                  </Button>
+                )}
+              </>
+            )}
           </div>
+        </section>
+        <SectionTemplate
+          title=""
+          description=""
+          className={styles.sectionPricing}
+        >
+          <div className={styles.flex}>
+            <Pricing
+              planTitle="Free"
+              price={0}
+              values={[
+                "Pełny dostęp do aplikacji",
+                "Nieograniczona liczba mentee",
+                "10% prowizji od spotkania",
+                "Brak darmowych spotkań",
+              ]}
+              selectedPlan={selectedPlan}
+              handleSelectPlan={handleSelectPlan}
+              canChangePlan={canSelectPlan("Free")}
+              currentPlan={subscriptionDetails?.planName || "Free"}
+            />
+            <Pricing
+              planTitle="Mid"
+              price={89}
+              values={[
+                "20 darmowych spotkań w miesiącu",
+                "Gwarancja stałej opłaty miesięcznej",
+                "Niższa prowizja: 5%",
+                "Pełny dostęp do aplikacji",
+                "Nieograniczona liczba mentee",
+              ]}
+              selectedPlan={selectedPlan}
+              handleSelectPlan={handleSelectPlan}
+              canChangePlan={canSelectPlan("Mid")}
+              currentPlan={subscriptionDetails?.planName || "Free"}
+            />
+            <Pricing
+              planTitle="Pro"
+              price={189}
+              values={[
+                "Darmowe spotkania bez limitu",
+                "Gwarancja stałej opłaty miesięcznej",
+                "Brak prowizji",
+                "Nieograniczona liczba mentee",
+              ]}
+              selectedPlan={selectedPlan}
+              handleSelectPlan={handleSelectPlan}
+              canChangePlan={canSelectPlan("Pro")}
+              currentPlan={subscriptionDetails?.planName || "Free"}
+            />
+          </div>
+        </SectionTemplate>
+        <div className={styles.subscriptionHistoryContainer}>
+          <MentorPaymentSubscription />
         </div>
-      </>
+      </div>
+    </>
   );
 };
